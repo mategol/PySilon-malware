269b2fc1656cf5dfcd8b8b4e2d103a9ba439bcf2
Mateusz Gołembowski <44233157+mategol@users.noreply.github.com>
2022-11-20 20:59:48 +0100

    Added function to send message each time script is started

--- main.py
+++ main.py
@@ -2,4 +2,13 @@ import discord
 
 client = discord.Client()
 bot_token = ''   # Paste here BOT-token
+
+channel_ids = {
+    'main': 831567586344697868
+}
+
+@client.event 
+async def on_ready():  
+    await client.get_channel(channel_ids['main']).send('New PC session')
+
 client.run(bot_token)
