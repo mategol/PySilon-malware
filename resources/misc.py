233c7eaa182d47a0e3875328b8e7545562f25350
Mateusz Gołembowski <44233157+mategol@users.noreply.github.com>
2022-11-21 17:48:45 +0100

    Added tree generating function

--- resources/misc.py
+++ resources/misc.py
@@ -1,4 +1,31 @@
 import datetime
+from pathlib import Path
+import sys
 
-def current_time():
-    return datetime.datetime.now().strftime('%d.%m.%Y_%H:%M')
+def current_time(seconds_also=False):
+    return datetime.datetime.now().strftime('%d.%m.%Y_%H.%M' if not seconds_also else '%d.%m.%Y_%H.%M.%S')
+
+def tree(dir_path: Path, level: int=-1, limit_to_directories: bool=False, length_limit: int=sys.maxsize):
+    global tree_files, tree_directories
+    space =  '    '
+    branch = '│   '
+    tee =    '├── '
+    last =   '└── '
+    dir_path = Path(dir_path)
+    level = int(-1)
+    limit_to_directories = False
+    def inner(dir_path: Path, prefix: str='', level=-1):
+        global tree_files, tree_directories
+        try:
+            contents = list(dir_path.iterdir())
+            pointers = [tee] * (len(contents) - 1) + [last]
+            for pointer, path in zip(pointers, contents):
+                if path.is_dir():
+                    yield prefix + pointer + path.name
+                    extension = branch if pointer == tee else space 
+                    yield from inner(path, prefix=prefix+extension, level=level-1)
+                elif not limit_to_directories:
+                    yield prefix + pointer + path.name
+        except Exception as err:
+            print(err)
+    return inner(dir_path, level=level)
