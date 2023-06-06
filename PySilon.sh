9e3da62ec8db02c82f637e8f7cb62c8351e0423e
Nick Roussis <nikosnick160@gmail.com>
2023-06-06 23:25:43 +0300

    fix enter inputs

--- PySilon.sh
+++ PySilon.sh
@@ -32,13 +32,15 @@ if [ $mode = 'c' ]; then
             sudo dnf update -y && sudo dnf install wine -y
         elif [ $package_manager = 'p' ]; then
             sudo pacman -Sy wine --noconfirm
+        elif [ -z $package_manager ]; then
+            echo -e "\e[34m[-] Enter was pressed, skipping.\e[0m"
         else
-            echo -e "\e[31m[x] Enter was pressed or invalid input was given, skipping.\e[0m"
+            echo -e "\e[31m[x] Invalid input was given, skipping.\e[0m"
         fi
     elif [ $wine_installed = 'y' ]; then
         :
     else
-        echo -e "\e[31m[x] Enter was pressed or invalid input was given, skipping.\e[0m"
+        echo -e "\e[31m[x] Enter was pressed or other invalid input was given, skipping.\e[0m"
     fi
 
     # Ask to download and install Python in Wine
@@ -52,8 +54,10 @@ if [ $mode = 'c' ]; then
         echo -e "\e[36m[#] Launching Python installer through Wine...\e[0m"
         echo -e "\e[1;35m[i] Make sure to add Python to PATH and go to \"Customize Installation > Next > Install for all users\" in the installer!\e[0m"
         wine ./python-3.10.8-amd64.exe # Change version to the version set in the above link
+    elif [ -z $install_python ]; then
+        echo -e "\e[34m[-] Enter was pressed, skipping.\e[0m"
     else
-        echo -e "\e[31m[x] Enter was pressed or invalid input was given, skipping.\e[0m"
+        echo -e "\e[31m[x] Invalid input was given, skipping.\e[0m"
     fi
 
     # Ask to create a new venv (or keep the existing, in case it already exists)
@@ -62,11 +66,15 @@ if [ $mode = 'c' ]; then
     if [ $create_venv = 'y' ]; then
         wine python -m pip install wheel setuptools
         wine python -m venv pysilon
+    elif [ -z $create_venv ]; then
+        echo -e "\e[34m[-] Enter was pressed, skipping.\e[0m"
+    else
+        echo -e "\e[31m[x] Invalid input was given, skipping.\e[0m"
     fi
 
     # Initializing venv
     echo -e "\e[36m[#] Initializing the virtual environment...\e[0m"
-    wine call "pysilon\\Scripts\\activate.bat"
+    wine call ".\\pysilon\\Scripts\\activate.bat" ###* Attention needed / Might not work (activate.bat does not get called) ###
 
     # Install requirements.txt
     echo -e "\e[36m[#] Installing PIP requirements.txt...\e[0m"
@@ -78,7 +86,7 @@ elif [ $mode = 'r' ]; then
     :
 # If no mode was selected, display error and continue
 else
-    echo -e "\e[31mInvalid input, proceeding to the execuion of builder.py anyways.\e[0m"
+    echo -e "\e[31m[x] Invalid input, proceeding to the execuion of builder.py anyways.\e[0m"
 fi
 
 # Running builder.py
