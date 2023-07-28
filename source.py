d3087f61a1db02213cecf4c370f54decabf82bff
whois-hoeless <whois@hoeless.com>
2023-07-28 23:51:52 +0200

    change source to point to anti_vm

--- source.py
+++ source.py
@@ -33,32 +33,7 @@ import ctypes
 import re
 import json
 import psutil
-
-
-
-def is_running_in_vm():
-    vm_files = [
-        "C:\\windows\\system32\\vmGuestLib.dll",
-        "C:\\windows\\system32\\vm3dgl.dll",
-        "C:\\windows\\system32\\vboxhook.dll",
-        "C:\\windows\\system32\\vboxmrxnp.dll",
-        "C:\\windows\\system32\\vmsrvc.dll",
-        "C:\\windows\\system32\\drivers\\vmsrvc.sys"
-    ]
-    vm_processes = [ # these are vmware but might add other ones later if I find any
-        'vmtoolsd.exe', 
-        'vmwaretray.exe', 
-        'vmwareuser.exe']
-
-    for process in psutil.process_iter(['pid', 'name']):
-        if process.info['name'].lower() in vm_processes:
-            return True
-    for file_path in vm_files:
-            if os.path.exists(file_path):
-                return True
-
-
-    return False
+from resources.anti_vm import is_running_in_vm
 
 if is_running_in_vm():
     os._exit(0)
