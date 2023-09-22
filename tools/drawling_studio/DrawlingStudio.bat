fd271605b2a0d9625ac8395cbc90da5709bb7b14
mategol <44233157+mategol@users.noreply.github.com>
2023-09-23 00:24:27 +0200

    Create DrawlingStudio.bat

---
+++ tools/drawling_studio/DrawlingStudio.bat
@@ -0,0 +1,27 @@
+@echo off
+title DrawlingStudio
+echo Initializing the virtual environment...
+python -m venv drawlingstudio
+cls
+call drawlingstudio\Scripts\activate.bat
+python -m pip install --upgrade pip
+pip install -r requirements.txt
+
+python main.py
+echo #===============================================================# 
+echo #                    Software terminated.                       # 
+echo #                                                               # 
+echo #   Give us a Star on Github, this would really help us grow!   # 
+echo #        https://github.com/mategol/PySilon-malware             # 
+echo #                                                               #
+echo #   Also, please don't send this malware using websites like    #
+echo # Workupload or googledrive because they will scan the malware  #
+echo # and keep track of it and other ocurrences, which will result  #
+echo # in more detections in the future, please send it to people in #
+echo # a zip archive with a password, or use services like anonfiles #
+echo #                                                               #
+echo #                         Thank You!                            #
+echo #===============================================================# 
+echo. 
+echo.
+pause
