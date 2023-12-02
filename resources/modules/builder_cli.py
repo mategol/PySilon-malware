8e18b119b5d5d34066fbf2e17a2f8180a1e202b9
mategol <mateusz.golembowski.2002@gmail.com>
2023-12-02 12:16:38 +0100

    Adapted command interpreter to take multiple arguments

--- resources/modules/builder_cli.py
+++ resources/modules/builder_cli.py
@@ -12,11 +12,28 @@ Type "help" for list of commands.
         self.get_command()
 
     def get_command(self):
-        self.issued_command = input('.').split()
-        match self.issued_command[0]:
+        self.issued_command = input('.')
+        match self.issued_command.split()[0]:
             case 'set':
-                self.issued_command[1] = self.issued_command[1].split('"')
-                print(f'    Command: {self.issued_command[0]}\n    Setting: {self.issued_command[1]}\n')
+                possible_settings = []
+                for argument in self.issued_command.split()[1:]:
+                    if argument.count('-') == 2 and argument.count('=') == 1 and argument.count('"') == 2:
+                        possible_settings.append(argument)
+                    else:
+                        self.error('Syntax', 'set')
+                        possible_settings = []
+                        self.get_command()
+                for setting in possible_settings:
+                    setting = setting.split('=')
+                    self.command_set(setting[0][2:], setting[1][1:-1])
+
+    def command_set(self, setting, value):
+        print(setting, value)
+
+    def error(self, type, help):
+        print('Error')
+                
+                
 
 
 CLI_Builder()
