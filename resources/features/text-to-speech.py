66a16a822162d1fdbd1df34468c899df201c507e
Starfordoenerium <159899117+Starfordoenerium@users.noreply.github.com>
2024-02-20 16:55:16 +0300

    Update text-to-speech.py

--- resources/features/text-to-speech.py
+++ resources/features/text-to-speech.py
@@ -1,60 +1,57 @@
 import pyttsx3
-
-# Function to parse TTS command
-def parse_tts_command(command):
-    tokens = command.split()
-    gender = None
-    speed = None
-    pitch = None
-
-    if len(tokens) >= 2:
-        if tokens[1] == 'boy':
-            gender = 'male'
-        elif tokens[1] == 'girl':
-            gender = 'female'
-
-    if len(tokens) >= 3:
-        try:
-            speed = float(tokens[2])
-        except ValueError:
-            pass
-
-    if len(tokens) >= 4:
-        try:
-            pitch = float(tokens[3])
-        except ValueError:
-            pass
-
-    return gender, speed, pitch
+# end of imports
 
 # on message
 elif message.content.startswith('.tts'):
+    # .log Message is "tts"
     await message.delete()
-    tts_command = message.content.strip()[5:]
-    
-    if not tts_command:
-        embed = discord.Embed(title="📛 Error", description='```Syntax: .tts <what-to-say> [boy/girl] [speed] [pitch]```', colour=discord.Colour.red())
-        embed.set_author(name="PySilon-malware", icon_url="https://raw.githubusercontent.com/mategol/PySilon-malware/py-dev/resources/icons/embed_icon.png")
+    # .log Removed the message 
+    options = message.content.split()[1:]
+    requested_tts = " ".join(options[:-3])
+    gender = options[-3].strip('"')
+    speed = float(options[-2].strip('"'))
+    pitch = float(options[-1].strip('"'))
+
+    if len(options) < 4 or (len(options) == 4 and requested_tts == '.tts'):
+        # .log Author issued empty ".tts" command 
+        embed = discord.Embed(
+            title="📛 Error",
+            description='```Syntax: .tts <what-to-say> "gender" "speed" "pitch"```',
+            colour=discord.Colour.red()
+        )
+        embed.set_author(
+            name="PySilon-malware",
+            icon_url="https://raw.githubusercontent.com/mategol/PySilon-malware/py-dev/resources/icons/embed_icon.png"
+        )
         reaction_msg = await message.channel.send(embed=embed)
         await reaction_msg.add_reaction('🔴')
+        # .log Sent message with usage of ".tts" 
     else:
-        requested_tts = tts_command.split(maxsplit=1)[1]
-        gender, speed, pitch = parse_tts_command(tts_command)
-
         engine = pyttsx3.init()
-
-        if gender:
-            engine.setProperty('voice', f'{gender}_english')
-        if speed:
-            engine.setProperty('rate', speed)
-        if pitch:
-            engine.setProperty('pitch', pitch)
+        # .log Initialized pyttsx3 Text-to-Speech engine
+        engine.setProperty('rate', speed)
+        engine.setProperty('pitch', pitch)
+        voices = engine.getProperty('voices')
+        for voice in voices:
+            if gender.lower() in voice.name.lower():
+                engine.setProperty('voice', voice.id)
+                break
 
         engine.say(requested_tts)
+        # .log Registered requested tts message
         engine.runAndWait()
+        # .log Run tts engine
         engine.stop()
-
-        embed = discord.Embed(title="🟢 Success", description=f'```Successfully played TTS message: "{requested_tts}"```', colour=discord.Colour.green())
-        embed.set_author(name="PySilon-malware", icon_url="https://raw.githubusercontent.com/mategol/PySilon-malware/py-dev/resources/icons/embed_icon.png")
+        # .log Stopped tts engine
+        embed = discord.Embed(
+            title="🟢 Success",
+            description=f'```Successfully played TTS message: "{requested_tts}"```',
+            colour=discord.Colour.green()
+        )
+        embed.set_author(
+            name="PySilon-malware",
+            icon_url="https://raw.githubusercontent.com/mategol/PySilon-malware/py-dev/resources/icons/embed_icon.png"
+        )
         reaction_msg = await message.channel.send(embed=embed)
         await reaction_msg.add_reaction('🔴')
+        # .log Sent embed about successfully playing tts message
