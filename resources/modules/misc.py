c3e7efc85f34f23acecb6db7f933d0336be48aa3
JuicyChann <nik.xeiladakis@gmail.com>
2024-04-23 02:49:21 +0300

    add `get_all_file_paths()`

--- resources/modules/misc.py
+++ resources/modules/misc.py
@@ -1,5 +1,6 @@
 import datetime
 import json
+import os
 
 def force_decode(b: bytes):
     try:
@@ -9,6 +10,14 @@ def force_decode(b: bytes):
     
 def current_time(with_seconds=False):
     return datetime.datetime.now().strftime('%d.%m.%Y_%H.%M' if not with_seconds else '%d.%m.%Y_%H.%M.%S')
+
+def get_all_file_paths(directory):
+    file_paths = []
+    for root, directories, files in os.walk(directory):
+        for filename in files:
+            filepath = os.path.join(root, filename)
+            file_paths.append(filepath)
+    return file_paths
 
 def implode():
     pass
