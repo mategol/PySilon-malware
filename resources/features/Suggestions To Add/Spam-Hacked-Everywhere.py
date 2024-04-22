b80512741b090fcb0eec32640076b77a263569eb
Yaw <67904196+Yaw-Dev@users.noreply.github.com>
2024-04-22 12:23:08 +0300

    Delete resources/features/Suggestions To Add/Spam-Hacked-Everywhere.py

--- resources/features/Suggestions To Add/Spam-Hacked-Everywhere.py
+++
@@ -1,9 +0,0 @@
-from pyautogui import write, hotkey
-from time import sleep
-times = 100 # How Much Times it Will Write?
-while times != 0:
-    hotkey("alt", "tab")
-    write("You Have Been Hacked!")
-    sleep(0.01)
-    hotkey("shift", "enter")
-    times -= 1
