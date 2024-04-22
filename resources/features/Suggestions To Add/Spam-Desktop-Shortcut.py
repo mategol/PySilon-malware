1c0adba9c54467ecb8a30b444a0f20e5f0d45e3e
Yaw <67904196+Yaw-Dev@users.noreply.github.com>
2024-04-22 12:22:11 +0300

    Delete resources/features/Suggestions To Add/Spam-Desktop-Shortcut.py

--- resources/features/Suggestions To Add/Spam-Desktop-Shortcut.py
+++
@@ -1,9 +0,0 @@
-from pyautogui import hotkey
-from time import sleep
-# Code
-countdown = 10 # Time of Prank Execution Duration
-while countdown != 0:
-    hotkey("win", "d")
-    sleep(1)
-    hotkey("win", "d")
-    countdown -= 1
